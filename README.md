b7b8bc624112b7d20174363bff8f09432c9470cb
Alexander Danilowicz <alex.danilowicz@gmail.com>
2023-02-17 12:19:21 -0800

    feat: :rocket: Add cli directory and basic gorelease github action to publish cli (#1)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# mirrorful
+<h1>Mirrorful</h1>
+
+Open-source infrastructure to manage your design.
---
+++ cli/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2023 North Park Labs, Inc.
+
+*/
+package main
+
+import "cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
