57522b86354eb9437d98ad0034124e76060436ae
teddarific <teddyni19@gmail.com>
2023-02-19 16:43:53 -0800

    CSS Export

--- editor/src/components/ColorPalette/ColorPaletteSection.tsx
+++ editor/src/components/ColorPalette/ColorPaletteSection.tsx
@@ -2,16 +2,31 @@ import { Heading } from 'components/core/Heading'
 import { ColorRow } from './ColorRow'
 import { useState } from 'react'
 import { TColorData } from '../../types'
+import { Button, Box } from '@chakra-ui/react'
+import { generateDefaultColorScale } from './utils'
 
 export function ColorPaletteSection() {
   const [colors, setColors] = useState<TColorData[]>([
     {
       name: 'Pink',
       hex: '#ED64A6',
+      scale: generateDefaultColorScale('#ED64A6'),
     },
   ])
 
   const [isAddingNewColor, setIsAddingNewColor] = useState<boolean>(false)
+  const [isExporting, setIsExporting] = useState<boolean>(false)
+
+  const handleExport = async () => {
+    setIsExporting(true)
+
+    await fetch('/api/export', {
+      method: 'POST',
+      body: JSON.stringify({
+        colorData: colors,
+      }),
+    })
+  }
 
   return (
     <div>
@@ -20,12 +35,32 @@ export function ColorPaletteSection() {
         {colors.map((color) => (
           <ColorRow key={color.name} colorData={color} />
         ))}
-        <button
-          className="mirrorful-button"
-          onClick={() => setIsAddingNewColor(false)}
+        <Box
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+          }}
         >
-          Add New Color
-        </button>
+          <Button
+            colorScheme="purple"
+            onClick={() => setIsAddingNewColor(false)}
+            css={{
+              width: 'fit-content',
+            }}
+          >
+            Add New Color
+          </Button>
+          <Button
+            colorScheme="purple"
+            onClick={handleExport}
+            css={{
+              width: 'fit-content',
+            }}
+          >
+            Export Config
+          </Button>
+        </Box>
       </div>
     </div>
   )
--- editor/src/types.ts
+++ editor/src/types.ts
@@ -1,4 +1,16 @@
 export type TColorData = {
   hex: string
   name: string
+  scale: {
+    '50': string
+    '100': string
+    '200': string
+    '300': string
+    '400': string
+    '500': string
+    '600': string
+    '700': string
+    '800': string
+    '900': string
+  }
 }
