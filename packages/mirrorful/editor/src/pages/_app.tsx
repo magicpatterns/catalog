27dab9718d38992cbe44ef283925a624c6a970f9
Teddy Ni <teddyni19@gmail.com>
2023-02-23 12:10:25 -0800

    Downgrade Next (#44)

--- packages/mirrorful/editor/src/pages/_app.tsx
+++ packages/mirrorful/editor/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import '../main.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
--- packages/mirrorful/editor/src/pages/index.tsx
+++ packages/mirrorful/editor/src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { Box, Button, useDisclosure } from '@chakra-ui/react'
 import { ColorPaletteSection } from 'components/ColorPalette/ColorPaletteSection'
 import { ExportSuccessModal } from 'components/ExportSuccessModal'
+import Head from 'next/head'
 import { useState, useEffect } from 'react'
 import { TColorData } from 'types'
-import '../main.css'
 
 export default function Editor() {
   const [colors, setColors] = useState<TColorData[]>([])
@@ -36,52 +36,64 @@ export default function Editor() {
   }
 
   return (
-    <Box>
-      <Box
-        css={{
-          position: 'fixed',
-          width: '100vw',
-          backdropFilter: 'blur(6px)',
-          backgroundColor: 'rgba(255, 255, 255, 0)',
-          zIndex: 5,
-          height: '56px',
-        }}
-      >
+    <>
+      <Head>
+        <title>Mirrorful Editor</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta
+          name="description"
+          content="Local editor for your design system"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Box>
         <Box
           css={{
-            display: 'flex',
-            padding: '8px 48px',
-            justifyContent: 'space-between',
+            position: 'fixed',
+            width: '100vw',
+            backdropFilter: 'blur(6px)',
+            backgroundColor: 'rgba(255, 255, 255, 0)',
+            zIndex: 5,
+            height: '56px',
           }}
         >
-          <Box>
-            <img src="/mirrorful_logo.png" style={{ height: '39px' }} />
+          <Box
+            css={{
+              display: 'flex',
+              padding: '8px 48px',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Box>
+              <img src="/mirrorful_logo.png" style={{ height: '39px' }} />
+            </Box>
+            <Button colorScheme="blue" onClick={handleExport}>
+              Export Config
+            </Button>
+            <Box />
           </Box>
-          <Button colorScheme="blue" onClick={handleExport}>
-            Export Config
-          </Button>
-          <Box />
+          <Box
+            css={{
+              height: '1px',
+              width: '100%',
+              background:
+                'linear-gradient(to right, #F5F5F5, #3F3F3F, #F5F5F5)',
+            }}
+          />
         </Box>
+
         <Box
-          css={{
-            height: '1px',
-            width: '100%',
-            background: 'linear-gradient(to right, #F5F5F5, #3F3F3F, #F5F5F5)',
+          padding={{
+            base: '80px 48px 16px 48px',
           }}
+        >
+          <ColorPaletteSection colors={colors} onUpdateColors={setColors} />
+        </Box>
+        <ExportSuccessModal
+          isOpen={isExportSuccessModalOpen}
+          onClose={onExportSuccessModalClose}
         />
       </Box>
-
-      <Box
-        padding={{
-          base: '80px 48px 16px 48px',
-        }}
-      >
-        <ColorPaletteSection colors={colors} onUpdateColors={setColors} />
-      </Box>
-      <ExportSuccessModal
-        isOpen={isExportSuccessModalOpen}
-        onClose={onExportSuccessModalClose}
-      />
-    </Box>
+    </>
   )
 }
