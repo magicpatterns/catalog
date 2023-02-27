3f8ef7d169e532f7bc471dcae7f856dbd1cd8ce7
Teddy Ni <teddyni19@gmail.com>
2023-02-26 21:24:14 -0800

    Redesign color palette section (#52)

--- packages/mirrorful/editor/src/components/ColorPalette/ColorRow.tsx
+++ packages/mirrorful/editor/src/components/ColorPalette/ColorRow.tsx
@@ -67,7 +67,7 @@ function ColorPanel({ title, hex }: { title: string; hex: string }) {
   )
 }
 
-export function ColorRow({
+export function ColorRow_DEPRECATED({
   colorData,
   onUpdateColorData,
   onDeleteColorData,
@@ -201,6 +201,214 @@ export function ColorRow({
             Delete Color
           </Button>
         </Stack> */}
+      </Box>
+      <EditColorModal
+        isOpen={isOpen}
+        onClose={(updatedColorData?: TColorData) => {
+          if (updatedColorData) {
+            onUpdateColorData(updatedColorData)
+          }
+          onClose()
+        }}
+        initialColorData={colorData}
+      />
+    </>
+  )
+}
+
+export function ColorRow({
+  colorData,
+  onUpdateColorData,
+  onDeleteColorData,
+  onSetAsPrimary,
+  onSetAsSecondary,
+}: {
+  colorData: TColorData
+  onUpdateColorData: (colorData: TColorData) => void
+  onDeleteColorData: () => void
+  onSetAsPrimary: () => void
+  onSetAsSecondary: () => void
+}) {
+  const colorScale = generateDefaultColorShades(colorData.base)
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <>
+      <Box css={{ display: 'flex' }}>
+        <Box css={{ width: 350 }}>
+          <Box>
+            <Heading fontWeight="extrabold" fontSize={28}>
+              {colorData.name}
+              {colorData.isPrimary && (
+                <Badge colorScheme="blue" css={{ marginLeft: '12px' }}>
+                  PRIMARY
+                </Badge>
+              )}
+              {colorData.isSecondary && (
+                <Badge colorScheme="green" css={{ marginLeft: '12px' }}>
+                  SECONDARY
+                </Badge>
+              )}
+            </Heading>
+            <Text
+              fontWeight="bold"
+              color="gray.600"
+              fontSize={16}
+              css={{ marginTop: '8px' }}
+            >
+              {colorData.base.toUpperCase()}
+            </Text>
+            <Text
+              fontWeight="bold"
+              color="gray.600"
+              fontSize={16}
+              css={{ marginTop: '4px' }}
+            >
+              {tinycolor(colorData.base).toRgbString()}
+            </Text>
+            <Text
+              fontWeight="bold"
+              color="gray.600"
+              fontSize={16}
+              css={{ marginTop: '4px' }}
+            >
+              {tinycolor(colorData.base).toHslString()}
+            </Text>
+          </Box>
+        </Box>
+        <Box css={{ display: 'flex' }}>
+          <Box
+            css={{
+              backgroundColor: colorData.base,
+              width: 200,
+              height: 200,
+              padding: '16px',
+              borderRadius: '12px 0 0 12px',
+              color: tinycolor(colorData.base).isDark() ? 'white' : 'black',
+            }}
+          >
+            <Text fontSize={14} fontWeight="black">
+              BASE
+            </Text>
+            <Text fontSize={22} fontWeight="medium">
+              {colorData.base.toUpperCase()}
+            </Text>
+          </Box>
+          <Box css={{ display: 'flex', flexDirection: 'column' }}>
+            <Box
+              css={{
+                backgroundColor: colorData.hover,
+                width: 200,
+                height: 100,
+                padding: '16px',
+                color: tinycolor(colorData.hover ?? '#ffffff').isDark()
+                  ? 'white'
+                  : 'black',
+                borderRadius: '0 12px 0 0',
+                border: colorData.hover ? 'none' : '1px dashed black',
+              }}
+            >
+              <Text fontSize={14} fontWeight="black">
+                HOVER
+              </Text>
+              <Text fontSize={colorData.hover ? 22 : 16} fontWeight="medium">
+                {colorData.hover ? colorData.hover.toUpperCase() : 'Not set.'}
+              </Text>
+            </Box>
+            <Box
+              css={{
+                backgroundColor: colorData.active,
+                width: 200,
+                height: 100,
+                padding: '16px',
+                color: tinycolor(colorData.active ?? '#ffffff').isDark()
+                  ? 'white'
+                  : 'black',
+                borderRadius: '0 0 12px 0',
+                border: colorData.active ? 'none' : '1px dashed black',
+              }}
+            >
+              <Text fontSize={14} fontWeight="black">
+                ACTIVE
+              </Text>
+              <Text fontSize={colorData.active ? 22 : 16} fontWeight="medium">
+                {colorData.active ? colorData.active.toUpperCase() : 'Not set.'}
+              </Text>
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          css={{ display: 'flex', alignItems: 'center', marginLeft: '64px' }}
+        >
+          <Box
+            css={{
+              display: 'flex',
+              width: 250,
+              flexWrap: 'wrap',
+              alignItems: 'center',
+            }}
+          >
+            {Object.keys(colorScale).map((weight) => (
+              <Box
+                key={weight}
+                style={{
+                  width: 50,
+                  height: 50,
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  fontSize: '14px',
+                  // @ts-ignore
+                  color: tinycolor(colorScale[weight]).isDark()
+                    ? 'white'
+                    : 'black',
+                  // @ts-ignore
+                  backgroundColor: colorScale[weight],
+                }}
+              >
+                {weight}
+              </Box>
+            ))}
+          </Box>
+        </Box>
+        <Stack css={{ marginLeft: '32px' }}>
+          <Button
+            css={{ marginTop: '16px' }}
+            backgroundColor={colorData.base}
+            color={tinycolor(colorData.base).isDark() ? 'white' : 'black'}
+            _hover={{
+              backgroundColor: colorData.hover ?? colorScale['600'],
+            }}
+            _active={{
+              backgroundColor: colorData.active ?? colorScale['700'],
+            }}
+            leftIcon={<Icon as={FiEdit} />}
+            onClick={() => {
+              onOpen()
+            }}
+          >
+            Edit
+          </Button>
+          <Button leftIcon={<Icon as={FiTrash} />} onClick={onDeleteColorData}>
+            Delete
+          </Button>
+          <Button
+            leftIcon={<Icon as={FiAward} />}
+            onClick={() => {
+              onSetAsPrimary()
+            }}
+          >
+            Set as Primary
+          </Button>
+          <Button
+            leftIcon={<Icon as={FiBookmark} />}
+            onClick={() => {
+              onSetAsSecondary()
+            }}
+          >
+            Set as Secondary
+          </Button>
+        </Stack>
       </Box>
       <EditColorModal
         isOpen={isOpen}
