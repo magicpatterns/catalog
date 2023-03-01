f4f17835eae869c20eecb8419bdd91ab9cbacc5a
Alex Danilowicz <alex.danilowicz@gmail.com>
2023-02-28 19:40:29 -0800

    Add posthog to better understand our users (#63)

--- packages/mirrorful/editor/src/pages/_app.tsx
+++ packages/mirrorful/editor/src/pages/_app.tsx
@@ -1,8 +1,33 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import '../main.css'
+import posthog from 'posthog-js'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+if (typeof window !== 'undefined') {
+  // This ensures that as long as we are client-side, posthog is always ready
+  posthog.init('phc_Fi1SAV5Xhkmrf5VwIweTTmZDNnUIWmXkvXr7naLsNVV', {
+    api_host: 'https://app.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.opt_out_capturing()
+    },
+  })
+}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => posthog.capture('$pageview')
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
   return (
     <ChakraProvider>
       <Component {...pageProps} />
