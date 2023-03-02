ef1be62e1f63e3b80a329af85c8f370f51316f18
alexdanilowicz <alex.danilowicz@gmail.com>
2023-03-01 18:27:24 -0800

    Release 2.0.22

---
+++ packages/mirrorful/editor/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
+
+module.exports = nextConfig
