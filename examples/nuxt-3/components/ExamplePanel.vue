aa7e368860bf9ad3c33eb850c535ad288751f9a0
Tobias Dossinger <tobias.dossinger@googlemail.com>
2023-03-05 23:51:32 +0100

    adds informations to nuxt config

--- examples/nuxt-3/components/ExamplePanel.vue
+++ examples/nuxt-3/components/ExamplePanel.vue
@@ -1,27 +1,32 @@
 <template>
   <div>
-    <h1>Example Panel</h1>
+    <h1 class="headline">Example Panel</h1>
     <div class="wrapper-panel">
-      <h2>Ui-Button-Component + Mirrorful-Variables in tailwind.config.js</h2>
+      <h2 class="description">
+        Ui-Button-Component + Mirrorful-Variables in tailwind.config.js
+      </h2>
       <ui-button :type="'button'" styling-tailwind>Hover me</ui-button>
     </div>
 
     <div class="wrapper-panel">
-      <h2>Ui-Button-Component + Mirrorful - From CSS-File</h2>
+      <h2 class="description">
+        Ui-Button-Component + Mirrorful - From CSS-File
+      </h2>
       <ui-button :type="'button'" styling-plain-css>Hover me</ui-button>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-h1 {
+.headline {
   @apply mb-4 text-2xl font-bold;
 }
 
-h2 {
-  @apply mb-4;
-}
 .wrapper-panel {
   @apply border px-6 py-6 rounded-sm mb-12;
+
+  .description {
+    @apply mb-4;
+  }
 }
 </style>
--- examples/nuxt-3/nuxt.config.ts
+++ examples/nuxt-3/nuxt.config.ts
@@ -1,10 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ['~/assets/css/main.css'],
-    postcss: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Nuxt 3 Example', 
+      meta: [
+        { name: 'description', content: 'Example of Nuxt 3 with Designsystem Mirrorful.' }
+      ],
+    },
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
