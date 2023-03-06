ba6f670611a79d9f71f59f75c22cf73f5e2fcd99
Tobias Dossinger <33021996+tobiasdossinger@users.noreply.github.com>
2023-03-06 04:09:42 +0100

    #98 - Adds nuxt 3 example (#98)
    * adds nuxt 3 example
    
    * Add .mirrorful folder
    
    * adds styling to component
    
    * refactoring nuxt-3 example
    
    * adds informations to nuxt config
    
    * fix typo
    
    * adds example to readme
    
    ---------
    
    Co-authored-by: alexdanilowicz <alex.danilowicz@gmail.com>

---
+++ examples/nuxt-3/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="wrapper">
+    <ExamplePanel />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  @apply flex flex-col items-center justify-center h-screen mx-4;
+}
+</style>
