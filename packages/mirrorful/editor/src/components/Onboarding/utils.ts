6dad22f409b00b067f525101664d8a88058a61e9
iamoghenerukevwe <50581464+iamoghenerukevwe@users.noreply.github.com>
2023-03-11 12:00:58 +0100

    fix color label as unknown

--- packages/mirrorful/editor/src/components/Onboarding/utils.ts
+++ packages/mirrorful/editor/src/components/Onboarding/utils.ts
@@ -2,6 +2,7 @@ import tinycolor from 'tinycolor2'
 import { TColorData } from 'types'
 
 const nameThatColor = ({ h, l, s }: { h: number; l: number; s: number }) => {
+  
   if (s <= 0.1 && l >= 0.9) {
     return 'White'
   } else if (l <= 0.15) {
@@ -10,25 +11,25 @@ const nameThatColor = ({ h, l, s }: { h: number; l: number; s: number }) => {
     return 'Gray'
   } else if ((h >= 0 && h <= 15) || h >= 346) {
     return 'Red'
-  } else if (h >= 16 && h <= 35) {
+  } else if (h >= 15 && h <= 35) {
     if (s < 0.9) {
       return 'Brown'
     } else {
       return 'Orange'
     }
-  } else if (h >= 36 && h <= 54) {
+  } else if (h >= 35 && h <= 54) {
     if (s < 0.9) {
       return 'Brown'
     } else {
       return 'Yellow'
     }
-  } else if (h >= 55 && h <= 165) {
+  } else if (h >= 54 && h <= 165) {
     return 'Green'
-  } else if (h >= 166 && h <= 260) {
+  } else if (h >= 165 && h <= 260) {
     return 'Blue'
-  } else if (h >= 261 && h <= 290) {
+  } else if (h >= 260 && h <= 290) {
     return 'Purple'
-  } else if (h >= 291 && h <= 345) {
+  } else if (h >= 290 && h <= 345) {
     return 'Pink'
   }
 
@@ -95,9 +96,10 @@ export function generatePalette(
       }
       color.name = `${color.name} ${index}`
     }
-
+  
     nameSet.add(color.name)
   })
-
+  console.log(randomizedColors)
   return randomizedColors
+  
 }
