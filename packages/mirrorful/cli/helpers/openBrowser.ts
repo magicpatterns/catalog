1faf1dc11d9965e69aa2f2cc7327bf144c7a337a
karanvirsb <karanvirboparai123@gmail.com>
2023-03-12 14:36:27 -0400

    removed res as it bloats the console

--- packages/mirrorful/cli/helpers/openBrowser.ts
+++ packages/mirrorful/cli/helpers/openBrowser.ts
@@ -2,11 +2,12 @@ import open from 'open'
 type props = { url: string; port: number }
 export default async function openBrowser({ url, port }: props) {
   try {
-    console.log(`Opening up browser to ${url}:${port}`)
-    const res = await open(`${url}:${port}`)
-
-    console.log('🚀 Opening Mirrorful in your browser! ', res)
+    await open(`${url}:${port}`)
+    console.log('🚀 Opening Mirrorful in your browser! ')
   } catch (error) {
-    console.log('Go to localhost:5050 to open Mirrorful. Could not open automatically: ', error)
+    console.log(
+      'Go to localhost:5050 to open Mirrorful. Could not open automatically: ',
+      error
+    )
   }
 }
