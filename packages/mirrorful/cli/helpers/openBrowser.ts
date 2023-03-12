37ca7d25b03260119f916e139841be3eca3d9792
karanvirsb <karanvirboparai123@gmail.com>
2023-03-12 09:52:31 -0400

    updated type of port

--- packages/mirrorful/cli/helpers/openBrowser.ts
+++ packages/mirrorful/cli/helpers/openBrowser.ts
@@ -1,5 +1,5 @@
 import open from 'open'
-type props = { url: string; port: string }
+type props = { url: string; port: number }
 export default async function openBrowser({ url, port }: props) {
   try {
     console.log(`Opening up browser to ${url}:${port}`)
