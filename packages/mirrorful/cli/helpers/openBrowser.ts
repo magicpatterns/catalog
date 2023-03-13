e57a45d3ff07534df14eb5d60564c05d5dab1a81
karanvirsb <karanvirboparai123@gmail.com>
2023-03-13 11:32:42 -0400

    added sleep and await along with countdown

--- packages/mirrorful/cli/helpers/openBrowser.ts
+++ packages/mirrorful/cli/helpers/openBrowser.ts
@@ -1,13 +1,32 @@
+import chalk from 'chalk'
 import open from 'open'
+import sleep from './sleep'
 type props = { url: string; port: number }
 export default async function openBrowser({ url, port }: props) {
   try {
+    await waitToOpenBrowser()
     await open(`${url}:${port}`)
-    console.log('🚀 Opening Mirrorful in your browser! ')
   } catch (error) {
     console.log(
       'Go to localhost:5050 to open Mirrorful. Could not open automatically: ',
       error
     )
   }
+}
+
+async function waitToOpenBrowser() {
+  let i = 5
+  process.stdout.write(
+    `🚀 Opening Mirrorful in your browser in ${chalk.red(`${i}`)}!`
+  )
+  let timer = setInterval(() => {
+    i--
+    process.stdout.clearLine(0)
+    process.stdout.cursorTo(0)
+    process.stdout.write(
+      `🚀 Opening Mirrorful in your browser in ${chalk.red(`${i}`)}!`
+    )
+  }, 1000)
+  await sleep(5000)
+  clearInterval(timer)
 }
