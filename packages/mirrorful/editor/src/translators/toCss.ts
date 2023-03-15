3796ba0414d1dbff7822eab6cb9c426606917021
zachsnoek <dev@zachsnoek.com>
2023-03-14 20:10:44 -0700

    refactor: add content translators

---
+++ packages/mirrorful/editor/src/translators/toCss.ts
@@ -0,0 +1,35 @@
+import { TTokens } from 'types'
+import { getKeys } from 'utils/getKeys'
+import { sanitizeName } from './sanitizeName'
+
+export const toCss = ({ colorData, typography }: TTokens): string => {
+  const content = [':root {']
+
+  colorData.forEach((color) => {
+    if (color.baseColor) {
+      content.push(`  --color-${sanitizeName(color.name)}: ${color.baseColor};`)
+    }
+
+    getKeys(color.variants).forEach((key) => {
+      if (color.variants[key]) {
+        content.push(
+          `  --color-${sanitizeName(color.name)}-${sanitizeName(key)}: ${
+            color.variants[key]
+          };`
+        )
+      }
+    })
+  })
+
+  typography.fontSizes.forEach((fontSize) => {
+    content.push(
+      `  --font-size-${sanitizeName(fontSize.name)}: ${fontSize.value}${
+        fontSize.unit
+      };`
+    )
+  })
+
+  content.push(`}`)
+
+  return content.join('\n')
+}
