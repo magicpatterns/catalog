59c6ccd7287d93814b8b0998a84333bf5673ceec
karanvirsb <karanvirboparai123@gmail.com>
2023-03-20 09:57:55 -0400

    created long poll end point

---
+++ packages/mirrorful/editor/src/pages/api/longPoll.ts
@@ -0,0 +1,11 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+// to handle the requests
+import pool from '../../utils/connections'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Transfer-Encoding', 'chunked')
+  res.setHeader('Content-Type', 'text/html')
+  pool.push(res)
+}
