32deddb0b66334244f410cdb09c7c5083c1ea4d3
karanvirsb <karanvirboparai123@gmail.com>
2023-03-21 09:37:06 -0400

    updated error for dom nesting p tag inside of a p tag

--- packages/core/src/components/ServerEndedMessage.tsx
+++ packages/core/src/components/ServerEndedMessage.tsx
@@ -3,17 +3,17 @@ import React from 'react'
 
 export default function ServerEndedMessage() {
   return (
-    <Grid placeContent={'center'} minHeight="100svh">
+    <Grid placeContent={'center'} minHeight="100svh" backgroundColor="#F2F2F2">
       <Text css={{ textAlign: 'center', fontSize: '5rem' }}>
         Mirrorful Server Has Been Shut Down!
       </Text>
       <Text css={{ textAlign: 'center', fontSize: '2rem', color: 'grey' }}>
         To start the server again, go to the terminal and type{' '}
-        <Text css={{ textAlign: 'center', color: 'purple' }}>
+        <Text css={{ textAlign: 'center', color: 'purple' }} as="span">
           yarn run mirrorful
-        </Text>
+        </Text>{' '}
         OR{' '}
-        <Text css={{ textAlign: 'center', color: 'purple' }}>
+        <Text css={{ textAlign: 'center', color: 'purple' }} as="span">
           npx mirrorful
         </Text>
       </Text>
