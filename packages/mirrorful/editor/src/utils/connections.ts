43a67ee5f4b7be5b35c01f2b1ce002e5dc00ea12
karanvirsb <karanvirboparai123@gmail.com>
2023-03-20 20:49:49 -0400

    commented out useless code

--- packages/mirrorful/editor/src/utils/connections.ts
+++ packages/mirrorful/editor/src/utils/connections.ts
@@ -3,13 +3,13 @@ import { NextApiResponse } from 'next'
 const connections: NextApiResponse[] = []
 
 // this sends ending message to all responses
-export function sendEndingMessage() {
-  console.log(connections)
-  connections.map((res, i) => {
-    console.log(i, 'ending')
-    res.write('exiting')
-    res.end()
-  })
-}
+// export function sendEndingMessage() {
+//   console.log(connections)
+//   connections.map((res, i) => {
+//     console.log(i, 'ending')
+//     res.write('exiting')
+//     res.end()
+//   })
+// }
 
 export default connections
