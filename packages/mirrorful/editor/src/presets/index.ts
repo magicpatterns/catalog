edf894069a35efadfa85168ac3f395867e144d27
karanvirsb <karanvirboparai123@gmail.com>
2023-03-21 16:02:02 -0400

    added file read and types as tokens TTokens

--- packages/mirrorful/editor/src/presets/index.ts
+++ packages/mirrorful/editor/src/presets/index.ts
@@ -1,14 +1,19 @@
+import { TTokens } from '@mirrorful/core/lib/types'
+import fs from 'fs'
+
 type props = {
   preset: 'linear' | 'facebook' | ''
 }
 
+const URL_TO_JSONS = './src/presets/model'
+
 export default async function getPreset({ preset }: props) {
-  console.log(`../../presets/model/${preset}.json`)
-  // const presetConfig = await fetch(
-  //   __dirname + `/packages/core/src/presets/model/${preset}.json`
-  // )
-  // console.log(presetConfig)
-  // return presetConfig
+  const presetConfig: unknown = await fs.promises.readFile(
+    `${URL_TO_JSONS}/${preset}.json`,
+    { encoding: 'utf-8' }
+  )
+
+  return presetConfig as { tokens: TTokens }
 }
 
 console.log(getPreset({ preset: 'linear' }))
