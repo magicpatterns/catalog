1e6084fdbaa5560e8efe727fb38fd9d7ee5b1fd4
karanvirsb <karanvirboparai123@gmail.com>
2023-03-20 20:49:39 -0400

    removed not needed

--- packages/mirrorful/editor/src/pages/api/endingPoll.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-// to handle the requests
-import { sendEndingMessage } from '../../utils/connections'
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  sendEndingMessage()
-  res.end()
-}
