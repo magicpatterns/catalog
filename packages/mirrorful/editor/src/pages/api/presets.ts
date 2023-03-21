9f7ae986018c2b111b29ae3737d42ab1b694383c
karanvirsb <karanvirboparai123@gmail.com>
2023-03-21 16:05:01 -0400

    added response json

--- packages/mirrorful/editor/src/pages/api/presets.ts
+++ packages/mirrorful/editor/src/pages/api/presets.ts
@@ -5,6 +5,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.write(await getPreset({ preset: 'linear' }))
+  res.json(await getPreset({ preset: 'linear' }))
+
   res.end()
 }
