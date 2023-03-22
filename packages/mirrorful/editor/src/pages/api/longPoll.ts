19d6fd45ba1ce4f1ce6d13ec48ddb6c38f8c619f
karanvirsb <karanvirboparai123@gmail.com>
2023-03-22 16:28:53 -0400

    removed pool

--- packages/mirrorful/editor/src/pages/api/longPoll.ts
+++ packages/mirrorful/editor/src/pages/api/longPoll.ts
@@ -1,10 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-// to handle the requests
-import pool from '../../utils/connections'
-
+// keeps requests open as it is long polling
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Transfer-Encoding', 'chunked')
   res.setHeader('Content-Type', 'text/html')
-  pool.push(res)
 }
