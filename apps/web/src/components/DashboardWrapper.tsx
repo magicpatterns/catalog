ac148073a2f2361b46ca59e0afa7913c2592d262
teddarific <teddyni19@gmail.com>
2023-03-29 11:39:34 -0700

    Init posthog on web

--- apps/web/src/components/DashboardWrapper.tsx
+++ apps/web/src/components/DashboardWrapper.tsx
@@ -3,6 +3,17 @@
 import { Dashboard } from '@mirrorful/core/components/Dashboard'
 import { defaultConfig, TConfig } from '@mirrorful/core/types'
 import { useLocalStorage } from '@web/hooks/useLocalStorage'
+import posthog from 'posthog-js'
+
+if (typeof window !== 'undefined') {
+  // This ensures that as long as we are client-side, posthog is always ready
+  posthog.init('phc_Fi1SAV5Xhkmrf5VwIweTTmZDNnUIWmXkvXr7naLsNVV', {
+    api_host: 'https://app.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.opt_out_capturing()
+    },
+  })
+}
 
 export function DashboardWrapper() {
   const [data, setData] = useLocalStorage<TConfig>(
