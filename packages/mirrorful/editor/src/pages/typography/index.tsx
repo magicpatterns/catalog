4e119627bc9a1783fb3b206983eaf5e8641d8fac
karanvirsb <karanvirboparai123@gmail.com>
2023-03-30 10:50:13 -0400

    added typography

--- packages/mirrorful/editor/src/pages/typography/index.tsx
+++ packages/mirrorful/editor/src/pages/typography/index.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { TypographySection } from '@mirrorful/core/lib/components/Typography/TypographySection'
 import useMirrorfulStore from 'src/zustand/useMirrorfulStore'
 function Typography() {
-  const fontSizes = useMirrorfulStore((state) => state.fontSizes)
+  const typography = useMirrorfulStore((state) => state.typography)
   return (
     <TypographySection
-      typography={{ fontSizes }}
+      typography={{ fontSizes: typography.fontSizes }}
       onUpdateTypography={() => {}}
     ></TypographySection>
   )
