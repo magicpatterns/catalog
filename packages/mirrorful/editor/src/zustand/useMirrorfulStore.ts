3face11bece8e4fcd3378427a778f0fe67b763b9
karanvirsb <karanvirboparai123@gmail.com>
2023-03-30 10:40:35 -0400

    added slices to the store

--- packages/mirrorful/editor/src/zustand/useMirrorfulStore.ts
+++ packages/mirrorful/editor/src/zustand/useMirrorfulStore.ts
@@ -1,8 +1,16 @@
 import { create } from 'zustand'
+import { ColorsSlice, createColorsSlice } from './colorsSlice'
+import { createFileTypesSlice, FileTypesSlice } from './fileTypesSlice'
+import { createShadowsSlice, ShadowsSlice } from './shadowsSlice'
 import { createTypographySlice, TypographySlice } from './typographySlice'
 
-const useMirrorfulStore = create<TypographySlice>()((...state) => ({
+const useMirrorfulStore = create<
+  TypographySlice & ColorsSlice & ShadowsSlice & FileTypesSlice
+>()((...state) => ({
+  ...createColorsSlice(...state),
   ...createTypographySlice(...state),
+  ...createShadowsSlice(...state),
+  ...createFileTypesSlice(...state),
 }))
 
 export default useMirrorfulStore
