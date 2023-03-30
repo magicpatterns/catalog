47fbfe2131b866f2e933d4d52a5a22993c4bdadb
karanvirsb <karanvirboparai123@gmail.com>
2023-03-30 10:53:05 -0400

    passed in shadows

--- packages/mirrorful/editor/src/pages/shadows/index.tsx
+++ packages/mirrorful/editor/src/pages/shadows/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { ShadowsSection } from '@mirrorful/core/lib/components/Shadows/ShadowsSection'
+import useMirrorfulStore from 'src/zustand/useMirrorfulStore'
 
 function Shadows() {
+  const { typography, colors, shadows, setTypography, fileTypes } =
+    useMirrorfulStore((state) => state)
   return (
-    <ShadowsSection onUpdateShadowData={() => {}} shadows={[]}></ShadowsSection>
+    <ShadowsSection
+      onUpdateShadowData={() => {}}
+      shadows={shadows}
+    ></ShadowsSection>
   )
 }
 
