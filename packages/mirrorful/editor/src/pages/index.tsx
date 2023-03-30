2f7bd652ef0eaed944fee7cd4a6b8e8f5c902d25
karanvirsb <karanvirboparai123@gmail.com>
2023-03-30 10:58:10 -0400

    created initial layout

--- packages/mirrorful/editor/src/pages/index.tsx
+++ packages/mirrorful/editor/src/pages/index.tsx
@@ -1,6 +1,8 @@
-import { Dashboard } from '@mirrorful/core/lib/components/Dashboard'
+import { Box } from '@chakra-ui/react'
+import { Dashboard, TTab } from '@mirrorful/core/lib/components/Dashboard'
 import { TConfig } from '@mirrorful/core/lib/types'
 import Head from 'next/head'
+import { platform } from 'os'
 
 export default function Editor() {
   return (
@@ -29,5 +31,32 @@ export default function Editor() {
         }}
       />
     </>
+  )
+}
+
+type props = { children: React.ReactNode }
+function Layout({ children }: props) {
+  return (
+    <Box css={{ width: '100%', minHeight: '100vh', display: 'flex' }}>
+      <Box css={{ width: '300px', position: 'fixed' }}>
+        <Sidebar
+          platform={platform}
+          activeTab={tab}
+          onSelectTab={(newTab: TTab) => setTab(newTab)}
+          onOpenSettings={() => onExportSettingsModalOpen()}
+          onExport={handleExport}
+          isDisabled={isLoading}
+        />
+      </Box>
+      <Box css={{ minWidth: '300px' }} />
+      <Box
+        css={{ backgroundColor: 'white', flexGrow: 1 }}
+        padding={{
+          base: '24px 48px',
+          md: '36px 72px',
+          lg: '48px 96px',
+        }}
+      ></Box>
+    </Box>
   )
 }
