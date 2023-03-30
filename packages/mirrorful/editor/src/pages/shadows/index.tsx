8edf9f88d4e26bacf3906df8e9ed448814c3bfac
karanvirsb <karanvirboparai123@gmail.com>
2023-03-30 10:53:37 -0400

    added updates for handling shadows

--- packages/mirrorful/editor/src/pages/shadows/index.tsx
+++ packages/mirrorful/editor/src/pages/shadows/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { ShadowsSection } from '@mirrorful/core/lib/components/Shadows/ShadowsSection'
 import useMirrorfulStore from 'src/zustand/useMirrorfulStore'
+import { TShadowData } from '@mirrorful/core/lib/types'
+import postStoreData from 'src/utils/postStoreData'
 
 function Shadows() {
-  const { typography, colors, shadows, setTypography, fileTypes } =
+  const { typography, colors, shadows, setShadows, fileTypes } =
     useMirrorfulStore((state) => state)
+  const handleUpdateShadows = async (data: TShadowData[]) => {
+    setShadows(data)
+    await postStoreData({
+      tokens: { colorData: colors, typography, shadows: data },
+      files: fileTypes,
+    })
+  }
   return (
     <ShadowsSection
-      onUpdateShadowData={() => {}}
+      onUpdateShadowData={handleUpdateShadows}
       shadows={shadows}
     ></ShadowsSection>
   )
