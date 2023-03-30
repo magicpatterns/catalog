caa194385d7a554ad5dc1aaece4c0b321c0fb4f9
karanvirsb <karanvirboparai123@gmail.com>
2023-03-30 10:04:42 -0400

    created shadows route

---
+++ packages/mirrorful/editor/src/pages/shadows/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ShadowsSection } from '@mirrorful/core/lib/components/Shadows/ShadowsSection'
+
+function Shadows() {
+  return (
+    <ShadowsSection onUpdateShadowData={() => {}} shadows={[]}></ShadowsSection>
+  )
+}
+
+export default Shadows
