978acf806d2ed37ca8863363115241b01b5292ef
karanvirsb <karanvirboparai123@gmail.com>
2023-03-30 10:01:06 -0400

    added typography route

---
+++ packages/mirrorful/editor/src/pages/typography/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TypographySection } from '@mirrorful/core/lib/components/Typography/TypographySection'
+function Typography() {
+  return (
+    <TypographySection
+      typography={{ fontSizes: [] }}
+      onUpdateTypography={() => {}}
+    ></TypographySection>
+  )
+}
+
+export default Typography
