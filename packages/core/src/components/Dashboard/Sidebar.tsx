bbfb48d53fa39a306609130ab64143603ddbe430
Teddy Ni <teddyni19@gmail.com>
2023-03-30 12:31:12 -0700

    Adding collapsible sidebar (#265)
    * added state for sidebar open
    
    * toggled the box
    
    * added condition to menu
    
    * added side open state
    
    * added button for close
    
    * set on click functionality
    
    * passed in state to sidebar
    
    * wrapped the side bar in a slide
    
    * moved menu button
    
    * aded onClick functionality
    
    * added motion
    
    * added hidden and initial
    
    * added animate
    
    * removed motion
    
    * toggled sidebar
    
    * added position fixed to button
    
    * Collapsible sidebar updates
    
    ---------
    
    Co-authored-by: karanvirsb <karanvirboparai123@gmail.com>

--- packages/core/src/components/Dashboard/Sidebar.tsx
+++ packages/core/src/components/Dashboard/Sidebar.tsx
@@ -1,17 +1,19 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons'
 import { Badge, Box, Icon, Stack, Text } from '@chakra-ui/react'
 import { VERSION } from '@core/utils/constants'
+import { motion } from 'framer-motion'
 import { useState } from 'react'
 import { IconType } from 'react-icons'
+import { CgSpaceBetween } from 'react-icons/cg'
 import {
-  FiAperture,
   FiBookOpen,
   FiFolder,
   FiGithub,
-  FiGrid,
   FiLayers,
   FiSettings,
   FiUnderline,
 } from 'react-icons/fi'
+import { MdOutlineColorLens } from 'react-icons/md'
 
 import { TPlatform, TTab } from '.'
 
@@ -19,6 +21,7 @@ function SidebarHeader({ label }: { label: string }) {
   return (
     <Text
       css={{
+        whiteSpace: 'nowrap',
         fontWeight: 900,
         fontSize: '0.9rem',
         color: 'gray',
@@ -36,6 +39,7 @@ function SidebarLink({
   onSelect,
   isComingSoon,
   isDisabled,
+  isCollapsed,
 }: {
   label: string
   icon: IconType
@@ -43,10 +47,12 @@ function SidebarLink({
   onSelect?: () => void
   isComingSoon?: boolean
   isDisabled?: boolean
+  isCollapsed?: boolean
 }) {
   const [isHovering, setIsHovering] = useState<boolean>(false)
 
-  const fontSize = '1rem'
+  const fontSize = '1.1rem'
+  const iconSize = '1.3rem'
 
   const isActiveState = !isDisabled && (isHovering || isActive)
 
@@ -58,6 +64,7 @@ function SidebarLink({
         color: isActiveState ? 'black' : 'gray',
         cursor: isComingSoon ? 'initial' : 'pointer',
         transition: '200ms',
+        height: '1.5rem',
       }}
       onMouseOver={() => {
         if (!isComingSoon || isDisabled) {
@@ -75,20 +82,24 @@ function SidebarLink({
         }
       }}
     >
-      <Icon as={icon} css={{ height: fontSize, width: fontSize }} />
-      <Text
-        css={{
-          fontWeight: 600,
-          fontSize,
-          marginLeft: '12px',
-        }}
-      >
-        {label}
-      </Text>
-      {isComingSoon && (
-        <Badge css={{ marginLeft: '12px' }} colorScheme="blue">
-          COMING SOON
-        </Badge>
+      <Icon as={icon} css={{ height: iconSize, width: iconSize }} />
+      {!isCollapsed && (
+        <>
+          <Text
+            css={{
+              fontWeight: 600,
+              fontSize,
+              marginLeft: '12px',
+            }}
+          >
+            {label}
+          </Text>
+          {isComingSoon && (
+            <Badge css={{ marginLeft: '12px' }} colorScheme="blue">
+              COMING SOON
+            </Badge>
+          )}
+        </>
       )}
     </Box>
   )
@@ -96,14 +107,16 @@ function SidebarLink({
 
 export function SidebarSection({
   header,
+  isCollapsed,
   children,
 }: {
   header: React.ReactNode
+  isCollapsed: boolean
   children: React.ReactNode
 }) {
   return (
     <Box>
-      {header}
+      <Box css={{ height: '2rem' }}>{!isCollapsed && header}</Box>
       <Stack spacing={'12px'} marginTop={'12px'}>
         {children}
       </Stack>
@@ -118,6 +131,8 @@ export function Sidebar({
   onOpenSettings,
   onExport,
   isDisabled,
+  isCollapsed,
+  onToggleCollapsed,
 }: {
   platform: TPlatform
   activeTab: string
@@ -125,6 +140,8 @@ export function Sidebar({
   onOpenSettings: () => void
   onExport: () => void
   isDisabled?: boolean
+  isCollapsed: boolean
+  onToggleCollapsed: () => void
 }) {
   return (
     <Box
@@ -136,16 +153,67 @@ export function Sidebar({
     >
       <Box
         css={{
+          position: 'absolute',
+          height: '100%',
+          right: -12,
+          width: '25px',
           display: 'flex',
           flexDirection: 'column',
-          padding: '36px 24px',
+          justifyContent: 'center',
+        }}
+      >
+        <Box
+          css={{
+            width: '25px',
+            height: '40px',
+            border: '1px solid lightgray',
+            backgroundColor: 'white',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 8,
+            transition: '200ms',
+            cursor: 'pointer',
+          }}
+          _hover={{
+            backgroundColor: '#F2F2F2',
+          }}
+          onClick={onToggleCollapsed}
+        >
+          {isCollapsed ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+        </Box>
+      </Box>
+      <Box
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          padding: isCollapsed ? '36px 12px' : '36px 24px',
           height: '100%',
           width: '100%',
           backgroundColor: '#F2F2F2',
         }}
       >
-        <Box>
-          <img src="/mirrorful_logo.png" style={{ width: '150px' }} />
+        <Box
+          css={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            height: '5rem',
+          }}
+        >
+          <Box>
+            {isCollapsed ? (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ delay: 0.2 }}
+              >
+                <img src="/simple_logo.png" style={{ width: '150px' }} />
+              </motion.div>
+            ) : (
+              <img src="/mirrorful_logo.png" style={{ width: '150px' }} />
+            )}
+          </Box>
         </Box>
         <Box
           css={{
@@ -156,14 +224,18 @@ export function Sidebar({
             marginTop: '20%',
           }}
         >
-          <SidebarSection header={<SidebarHeader label="Primitives" />}>
+          <SidebarSection
+            header={<SidebarHeader label="Primitives" />}
+            isCollapsed={isCollapsed}
+          >
             <SidebarLink
               key="sidebar-colors"
               label="Colors"
-              icon={FiAperture}
+              icon={MdOutlineColorLens}
               isActive={activeTab === 'colors'}
               onSelect={() => onSelectTab('colors')}
               isDisabled={isDisabled}
+              isCollapsed={isCollapsed}
             />
 
             <SidebarLink
@@ -173,6 +245,7 @@ export function Sidebar({
               isActive={activeTab === 'typography'}
               onSelect={() => onSelectTab('typography')}
               isDisabled={isDisabled}
+              isCollapsed={isCollapsed}
             />
 
             <SidebarLink
@@ -182,13 +255,15 @@ export function Sidebar({
               isActive={activeTab === 'shadows'}
               onSelect={() => onSelectTab('shadows')}
               isDisabled={isDisabled}
+              isCollapsed={isCollapsed}
             />
             <SidebarLink
               key="sidebar-spacing"
               label="Spacing"
-              icon={FiGrid}
+              icon={CgSpaceBetween}
               isComingSoon
               isDisabled={isDisabled}
+              isCollapsed={isCollapsed}
             />
           </SidebarSection>
 
@@ -200,11 +275,15 @@ export function Sidebar({
             />
           </SidebarSection> */}
 
-          <SidebarSection header={<SidebarHeader label="Export" />}>
+          <SidebarSection
+            header={<SidebarHeader label="Export" />}
+            isCollapsed={isCollapsed}
+          >
             <SidebarLink
               label="Export Tokens"
               icon={FiFolder}
               onSelect={() => onExport()}
+              isCollapsed={isCollapsed}
             />
             {platform === 'package' && (
               <SidebarLink
@@ -212,18 +291,23 @@ export function Sidebar({
                 icon={FiSettings}
                 onSelect={() => onOpenSettings()}
                 isDisabled={isDisabled}
+                isCollapsed={isCollapsed}
               />
             )}
           </SidebarSection>
 
           <Box>
-            <SidebarSection header={<SidebarHeader label="Resources" />}>
+            <SidebarSection
+              header={<SidebarHeader label="Resources" />}
+              isCollapsed={isCollapsed}
+            >
               <SidebarLink
                 label="Documentation"
                 icon={FiBookOpen}
                 onSelect={() =>
                   window.open('https://mirrorful.com/docs', '_blank')
                 }
+                isCollapsed={isCollapsed}
               />
               <SidebarLink
                 label="Github"
@@ -234,12 +318,15 @@ export function Sidebar({
                     '_blank'
                   )
                 }
+                isCollapsed={isCollapsed}
               />
             </SidebarSection>
-            <Box css={{ marginTop: '32px' }}>
-              <Text fontWeight="bold" color="gray.400" fontSize={'0.8rem'}>
-                {platform === 'web' ? 'WEB' : 'PACKAGE'} BETA {VERSION}
-              </Text>
+            <Box css={{ marginTop: '32px', height: '2rem' }}>
+              {!isCollapsed && (
+                <Text fontWeight="bold" color="gray.400" fontSize={'0.8rem'}>
+                  {platform === 'web' ? 'WEB' : 'PACKAGE'} BETA {VERSION}
+                </Text>
+              )}
             </Box>
           </Box>
         </Box>
