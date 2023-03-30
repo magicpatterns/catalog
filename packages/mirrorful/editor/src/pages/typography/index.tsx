e2dede834c7b72f21dbd7114406a044898295f0d
karanvirsb <karanvirboparai123@gmail.com>
2023-03-30 10:51:23 -0400

    added handle update

--- packages/mirrorful/editor/src/pages/typography/index.tsx
+++ packages/mirrorful/editor/src/pages/typography/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { TypographySection } from '@mirrorful/core/lib/components/Typography/TypographySection'
 import useMirrorfulStore from 'src/zustand/useMirrorfulStore'
+import { TTypographyData } from '@mirrorful/core/lib/types'
+import postStoreData from 'src/utils/postStoreData'
 function Typography() {
-  const typography = useMirrorfulStore((state) => state.typography)
+  const { typography, colors, shadows, setTypography, fileTypes } =
+    useMirrorfulStore((state) => state)
+  const handleUpdateTypography = async (data: TTypographyData) => {
+    setTypography(data)
+    await postStoreData({
+      tokens: { colorData: colors, typography: data, shadows },
+      files: fileTypes,
+    })
+  }
   return (
     <TypographySection
       typography={{ fontSizes: typography.fontSizes }}
-      onUpdateTypography={() => {}}
+      onUpdateTypography={handleUpdateTypography}
     ></TypographySection>
   )
 }
