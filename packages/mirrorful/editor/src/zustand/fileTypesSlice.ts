589e33c1a0c25e49e207903e978829fb8c4983af
karanvirsb <karanvirboparai123@gmail.com>
2023-03-30 10:39:24 -0400

    added file types state

---
+++ packages/mirrorful/editor/src/zustand/fileTypesSlice.ts
@@ -0,0 +1,20 @@
+import {
+  TExportFileType,
+  TFontSizeVariant,
+  TTypographyData,
+} from '@mirrorful/core/lib/types'
+import { StateCreator } from 'zustand'
+
+export interface FileTypesSlice {
+  fileTypes: TExportFileType[]
+  setFileTypes: (newState: TExportFileType[]) => void
+}
+export const createFileTypesSlice: StateCreator<
+  FileTypesSlice,
+  [],
+  [],
+  FileTypesSlice
+> = (set) => ({
+  fileTypes: [],
+  setFileTypes: (newState) => set((state) => ({ fileTypes: newState })),
+})
