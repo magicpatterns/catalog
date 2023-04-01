075c7db0a383abd69ad3c57b0f0f4a7ccf558181
karanvirsb <karanvirboparai123@gmail.com>
2023-04-01 10:06:01 -0400

    moved functions to core

--- packages/mirrorful/editor/src/utils/fetchStoreData.ts
+++
@@ -1,8 +0,0 @@
-import { TConfig } from '@mirrorful/core/lib/types'
-
-export default async function fetchStoreData() {
-  const response = await fetch('/api/config')
-  const data: TConfig = await response.json()
-
-  return data
-}
