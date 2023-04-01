cd1967bf3167e235b6e0245b864e35a9172311de
karanvirsb <karanvirboparai123@gmail.com>
2023-04-01 11:03:51 -0400

    passed platform to sidebar

--- packages/core/src/components/Layout.tsx
+++ packages/core/src/components/Layout.tsx
@@ -73,7 +73,7 @@ export function Layout({
           setShowOnBoarding(false)
           setShouldForceSkipOnboarding(true)
         }}
-        platform={'package'}
+        platform={platform}
       />
     )
   }
