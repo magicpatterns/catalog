0cb5ab83563d975a00e5da770f46ed5bfd2c37a5
karanvirsb <karanvirboparai123@gmail.com>
2023-04-01 11:28:34 -0400

    removed redirect

--- packages/mirrorful/editor/next.config.js
+++ packages/mirrorful/editor/next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  redirects: async () => {
-    return [
-      {
-        source: '/colors',
-        destination: '/',
-        permanent: true,
-      },
-    ]
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
