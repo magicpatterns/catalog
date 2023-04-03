4e7fba20507e8dbaed4ff2e37159ac71d955d0f4
karanvirsb <karanvirboparai123@gmail.com>
2023-04-03 14:06:42 -0400

    updated shadows to link shadows core

--- packages/mirrorful/editor/src/pages/shadows/index.tsx
+++ packages/mirrorful/editor/src/pages/shadows/index.tsx
@@ -1,30 +1,38 @@
-import { ShadowsSection } from '@mirrorful/core/lib/components/Shadows/ShadowsSection'
-import useMirrorfulStore, {
-  MirrorfulState,
-} from '@mirrorful/core/lib/store/useMirrorfulStore'
-import { TShadowData } from '@mirrorful/core/lib/types'
-import postStoreData from '@mirrorful/core/lib/utils/postStoreData'
+// import { ShadowsSection } from '@mirrorful/core/lib/components/Shadows/ShadowsSection'
+// import useMirrorfulStore, {
+//   MirrorfulState,
+// } from '@mirrorful/core/lib/store/useMirrorfulStore'
+// import { TShadowData } from '@mirrorful/core/lib/types'
+// import postStoreData from '@mirrorful/core/lib/utils/postStoreData'
+// import React from 'react'
+// import { Layout } from 'src/components/Layout'
+
+// function Shadows() {
+//   const { typography, colors, shadows, setShadows, fileTypes } =
+//     useMirrorfulStore((state: MirrorfulState) => state)
+//   const handleUpdateShadows = async (data: TShadowData[]) => {
+//     setShadows(data)
+//     await postStoreData({
+//       tokens: { colorData: colors, typography, shadows: data },
+//       files: fileTypes,
+//     })
+//   }
+//   return (
+//     <Layout>
+//       <ShadowsSection
+//         onUpdateShadowData={handleUpdateShadows}
+//         shadows={shadows}
+//       />
+//     </Layout>
+//   )
+// }
+
+// export default Shadows
+
+import Shadows from '@mirrorful/core/lib/pages/shadows'
 import React from 'react'
-import { Layout } from 'src/components/Layout'
+import postStoreData from 'src/utils/postStoreData'
 
-function Shadows() {
-  const { typography, colors, shadows, setShadows, fileTypes } =
-    useMirrorfulStore((state: MirrorfulState) => state)
-  const handleUpdateShadows = async (data: TShadowData[]) => {
-    setShadows(data)
-    await postStoreData({
-      tokens: { colorData: colors, typography, shadows: data },
-      files: fileTypes,
-    })
-  }
-  return (
-    <Layout>
-      <ShadowsSection
-        onUpdateShadowData={handleUpdateShadows}
-        shadows={shadows}
-      />
-    </Layout>
-  )
+export default function ShadowsPage() {
+  return <Shadows postStoreData={postStoreData} />
 }
-
-export default Shadows
