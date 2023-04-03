92a3db409b61ee73988721e9c3e9d28dd10a31b3
karanvirsb <karanvirboparai123@gmail.com>
2023-04-03 14:08:24 -0400

    updated typography to link core

--- packages/mirrorful/editor/src/pages/typography/index.tsx
+++ packages/mirrorful/editor/src/pages/typography/index.tsx
@@ -1,29 +1,39 @@
-import { TypographySection } from '@mirrorful/core/lib/components/Typography/TypographySection'
-import useMirrorfulStore, {
-  MirrorfulState,
-} from '@mirrorful/core/lib/store/useMirrorfulStore'
-import { TTypographyData } from '@mirrorful/core/lib/types'
-import postStoreData from '@mirrorful/core/lib/utils/postStoreData'
+// import { TypographySection } from '@mirrorful/core/lib/components/Typography/TypographySection'
+// import useMirrorfulStore, {
+//   MirrorfulState,
+// } from '@mirrorful/core/lib/store/useMirrorfulStore'
+// import { TTypographyData } from '@mirrorful/core/lib/types'
+// import postStoreData from '@mirrorful/core/lib/utils/postStoreData'
+// import React from 'react'
+// import { Layout } from 'src/components/Layout'
+// function Typography() {
+//   const { typography, colors, shadows, setTypography, fileTypes } =
+//     useMirrorfulStore((state: MirrorfulState) => state)
+//   const handleUpdateTypography = async (data: TTypographyData) => {
+//     setTypography(data)
+//     await postStoreData({
+//       tokens: { colorData: colors, typography: data, shadows },
+//       files: fileTypes,
+//     })
+//   }
+//   return (
+//     <Layout>
+//       <TypographySection
+//         typography={{ fontSizes: typography.fontSizes }}
+//         onUpdateTypography={handleUpdateTypography}
+//       />
+//     </Layout>
+//   )
+// }
+
+// export default Typography
+
+import Typography from '@mirrorful/core/lib/pages/typography'
 import React from 'react'
-import { Layout } from 'src/components/Layout'
-function Typography() {
-  const { typography, colors, shadows, setTypography, fileTypes } =
-    useMirrorfulStore((state: MirrorfulState) => state)
-  const handleUpdateTypography = async (data: TTypographyData) => {
-    setTypography(data)
-    await postStoreData({
-      tokens: { colorData: colors, typography: data, shadows },
-      files: fileTypes,
-    })
-  }
-  return (
-    <Layout>
-      <TypographySection
-        typography={{ fontSizes: typography.fontSizes }}
-        onUpdateTypography={handleUpdateTypography}
-      />
-    </Layout>
-  )
+import postStoreData from 'src/utils/postStoreData'
+
+function TypographyPage() {
+  return <Typography postStoreData={postStoreData} />
 }
 
-export default Typography
+export default TypographyPage
