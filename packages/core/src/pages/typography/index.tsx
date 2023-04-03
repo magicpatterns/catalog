b5ecfe229a9286fc4827366033a4888567c84fb5
karanvirsb <karanvirboparai123@gmail.com>
2023-04-03 14:07:27 -0400

    updated typography to the core

---
+++ packages/core/src/pages/typography/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Layout } from 'src/components/Layout'
+
+import { TypographySection } from '../../components/Typography/TypographySection'
+import useMirrorfulStore, {
+  MirrorfulState,
+} from '../../store/useMirrorfulStore'
+import { TConfig, TTypographyData } from '../../types'
+function Typography({
+  postStoreData,
+}: {
+  postStoreData: (data: TConfig) => Promise<void>
+}) {
+  const { typography, colors, shadows, setTypography, fileTypes } =
+    useMirrorfulStore((state: MirrorfulState) => state)
+  const handleUpdateTypography = async (data: TTypographyData) => {
+    setTypography(data)
+    await postStoreData({
+      tokens: { colorData: colors, typography: data, shadows },
+      files: fileTypes,
+    })
+  }
+  return (
+    <Layout>
+      <TypographySection
+        typography={{ fontSizes: typography.fontSizes }}
+        onUpdateTypography={handleUpdateTypography}
+      />
+    </Layout>
+  )
+}
+
+export default Typography
