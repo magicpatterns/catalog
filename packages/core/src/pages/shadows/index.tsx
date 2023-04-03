c55f00c5930e69f998b81b31d66a2844ca7de55b
karanvirsb <karanvirboparai123@gmail.com>
2023-04-03 14:05:33 -0400

    updated shadows to core

---
+++ packages/core/src/pages/shadows/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Layout } from 'src/components/Layout'
+
+import { ShadowsSection } from '../../components/Shadows/ShadowsSection'
+import useMirrorfulStore, {
+  MirrorfulState,
+} from '../../store/useMirrorfulStore'
+import { TConfig, TShadowData } from '../../types'
+
+function Shadows({
+  postStoreData,
+}: {
+  postStoreData: (data: TConfig) => Promise<void>
+}) {
+  const { typography, colors, shadows, setShadows, fileTypes } =
+    useMirrorfulStore((state: MirrorfulState) => state)
+  const handleUpdateShadows = async (data: TShadowData[]) => {
+    setShadows(data)
+    await postStoreData({
+      tokens: { colorData: colors, typography, shadows: data },
+      files: fileTypes,
+    })
+  }
+  return (
+    <Layout>
+      <ShadowsSection
+        onUpdateShadowData={handleUpdateShadows}
+        shadows={shadows}
+      />
+    </Layout>
+  )
+}
+
+export default Shadows
