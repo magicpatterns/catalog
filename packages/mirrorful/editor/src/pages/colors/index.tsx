a3f41bec8ee88f7557acae21f712671fd30778d0
karanvirsb <karanvirboparai123@gmail.com>
2023-04-03 14:03:32 -0400

    commented out prev for return. Added new color from core

--- packages/mirrorful/editor/src/pages/colors/index.tsx
+++ packages/mirrorful/editor/src/pages/colors/index.tsx
@@ -1,33 +1,43 @@
-import { ColorPaletteSection } from '@mirrorful/core/lib/components/ColorPalette/ColorPaletteSection'
-import useMirrorfulStore, {
-  MirrorfulState,
-} from '@mirrorful/core/lib/store/useMirrorfulStore'
-import { TColorData } from '@mirrorful/core/lib/types'
-import postStoreData from '@mirrorful/core/lib/utils/postStoreData'
-import { Layout } from 'src/components/Layout'
+// import { ColorPaletteSection } from '@mirrorful/core/lib/components/ColorPalette/ColorPaletteSection'
+// import useMirrorfulStore, {
+//   MirrorfulState,
+// } from '@mirrorful/core/lib/store/useMirrorfulStore'
+// import { TColorData } from '@mirrorful/core/lib/types'
+// import postStoreData from '@mirrorful/core/lib/utils/postStoreData'
+// import { Layout } from 'src/components/Layout'
 
-export default function Colors() {
-  const { colors, typography, shadows, fileTypes, setColors } =
-    useMirrorfulStore((state: MirrorfulState) => state)
+// export default function Colors() {
+//   const { colors, typography, shadows, fileTypes, setColors } =
+//     useMirrorfulStore((state: MirrorfulState) => state)
 
-  const handleUpdateColors = async (data: TColorData[]) => {
-    setColors(data)
-    await postStoreData({
-      tokens: {
-        typography,
-        colorData: data,
-        shadows,
-      },
-      files: fileTypes,
-    })
-  }
+//   const handleUpdateColors = async (data: TColorData[]) => {
+//     setColors(data)
+//     await postStoreData({
+//       tokens: {
+//         typography,
+//         colorData: data,
+//         shadows,
+//       },
+//       files: fileTypes,
+//     })
+//   }
 
-  return (
-    <Layout>
-      <ColorPaletteSection
-        colors={colors}
-        onUpdateColors={handleUpdateColors}
-      />
-    </Layout>
-  )
+//   return (
+//     <Layout>
+//       <ColorPaletteSection
+//         colors={colors}
+//         onUpdateColors={handleUpdateColors}
+//       />
+//     </Layout>
+//   )
+// }
+
+import Colors from '@mirrorful/core/lib/pages/colors'
+import React from 'react'
+import postStoreData from 'src/utils/postStoreData'
+
+function ColorsPage() {
+  return <Colors postStoreData={postStoreData} />
 }
+
+export default ColorsPage
