a451b147c8cf94d84f4373ccdfbeaf467ce55ece
karanvirsb <karanvirboparai123@gmail.com>
2023-04-04 08:05:29 -0400

    created typography page

---
+++ apps/web/src/app/typography/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+import TypographyPage from '@core/pages/typography'
+import { defaultConfig, TConfig } from '@core/types'
+import LayoutWrapper from '@web/components/LayoutWrapper'
+import { useLocalStorage } from '@web/hooks/useLocalStorage'
+import React from 'react'
+
+function Typography() {
+  const [data, setData] = useLocalStorage<TConfig>(
+    'mirrorfulConfigData',
+    defaultConfig
+  )
+  return (
+    <LayoutWrapper>
+      <TypographyPage
+        postStoreData={async (newData: TConfig) => {
+          setData(newData)
+        }}
+      />
+    </LayoutWrapper>
+  )
+}
+
+export default Typography
