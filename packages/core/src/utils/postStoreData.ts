78b2a6e8af4c1dd628dea680e1def5ceda1d189e
karanvirsb <karanvirboparai123@gmail.com>
2023-04-04 11:26:47 -0400

    removed

--- packages/core/src/utils/postStoreData.ts
+++
@@ -1,8 +0,0 @@
-import { TConfig } from '../types'
-
-export default async function postStoreData(data: TConfig) {
-  await fetch('/api/export', {
-    method: 'POST',
-    body: JSON.stringify(data),
-  })
-}
