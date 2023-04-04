efaf23edcc7b2501da23868d9072e13086bb5001
karanvirsb <karanvirboparai123@gmail.com>
2023-04-04 17:12:03 -0400

    updated import path TPlatform

--- packages/core/src/components/Onboarding/constants.ts
+++ packages/core/src/components/Onboarding/constants.ts
@@ -1,4 +1,4 @@
-import { TPlatform } from '../Dashboard'
+import { TPlatform } from '../Layout'
 
 export const getNumberOfStepsInOnboardingFlow = (platform: TPlatform) =>
   platform === 'web' ? `05` : `07`
