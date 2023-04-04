8536d0a4d33244c2b311feb3f756846a64ae4e89
karanvirsb <karanvirboparai123@gmail.com>
2023-04-03 22:48:36 -0400

    added layout wrapper

---
+++ apps/web/src/components/LayoutWrapper.tsx
@@ -0,0 +1,23 @@
+import Layout from '@core/components/Layout'
+import { defaultConfig, TConfig } from '@core/types'
+import { useLocalStorage } from '@web/hooks/useLocalStorage'
+import React from 'react'
+
+function LayoutWrapper({ children }: { children: React.ReactNode }) {
+  const [data, setData] = useLocalStorage<TConfig>(
+    'mirrorfulConfigData',
+    defaultConfig
+  )
+  return (
+    <Layout
+      postStoreData={async (newData: TConfig) => {
+        setData(newData)
+      }}
+      platform="web"
+    >
+      {children}
+    </Layout>
+  )
+}
+
+export default LayoutWrapper
