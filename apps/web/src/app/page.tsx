6fc8b30f0698ed2ff589c7b1f61cc5b2203b298b
karanvirsb <karanvirboparai123@gmail.com>
2023-04-04 08:10:54 -0400

    updated description

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,11 +3,6 @@ import { DashboardWrapper } from '@web/components/DashboardWrapper'
 import Head from 'next/head'
 import { useRouter } from 'next/navigation'
 
-// export const metadata = {
-//   title: 'Mirrorful Editor',
-//   description: `Create, edit, and manage your app's theme.`,
-// }
-
 export default function Home() {
   const router = useRouter()
   router.replace('/colors')
@@ -16,7 +11,10 @@ export default function Home() {
     <Head>
       <title>Mirrorful Editor</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Local editor for your design system" />
+      <meta
+        name="description"
+        content="Create, edit, and manage your app's theme."
+      />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   )
