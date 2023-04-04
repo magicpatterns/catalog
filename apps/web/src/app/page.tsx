7b61387263b02f92630e1a872608e5f8643f0364
karanvirsb <karanvirboparai123@gmail.com>
2023-04-04 07:50:51 -0400

    updated added use client and router replace

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,24 @@
+'use client'
 import { DashboardWrapper } from '@web/components/DashboardWrapper'
+import Head from 'next/head'
+import { useRouter } from 'next/navigation'
 
-export const metadata = {
-  title: 'Mirrorful Editor',
-  description: `Create, edit, and manage your app's theme.`,
-}
+// export const metadata = {
+//   title: 'Mirrorful Editor',
+//   description: `Create, edit, and manage your app's theme.`,
+// }
 
 export default function Home() {
-  return <DashboardWrapper />
+  const router = useRouter()
+  router.replace('/colors')
+
+  return (
+    <Head>
+      <title>Mirrorful Editor</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Local editor for your design system" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+  // <DashboardWrapper />
 }
