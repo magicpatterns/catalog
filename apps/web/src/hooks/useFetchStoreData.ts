82f9a6571e2c78c25dd8bfcac29525190a7583dd
Teddy Ni <teddyni19@gmail.com>
2023-04-08 04:50:53 -0700

    Fix existing data issue (#293)

--- apps/web/src/hooks/useFetchStoreData.ts
+++ apps/web/src/hooks/useFetchStoreData.ts
@@ -3,7 +3,10 @@ import { defaultConfig, TConfig } from '@core/types'
 import { useLocalStorage } from './useLocalStorage'
 
 export function useFetchStoreData() {
-  const [data] = useLocalStorage<TConfig>('mirrorfulConfigData', defaultConfig)
+  const [data] = useLocalStorage<TConfig>(
+    'mirrorfulConfigDataV2',
+    defaultConfig
+  )
 
   const fetchStoreData = async () => {
     return data
