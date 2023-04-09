f8de021602239b92df6bb866dbe18ff38af8a7ef
karanvirsb <karanvirboparai123@gmail.com>
2023-04-09 09:55:08 -0400

    updated path

--- packages/mirrorful/utils/addToTailwindConfig.ts
+++ packages/mirrorful/utils/addToTailwindConfig.ts
@@ -168,7 +168,7 @@ export async function addToTailwindConfig() {
     let mirrorfulImport = ''
     if (!tailwindFile.includes('.mirrorful/theme_cjs.js')) {
       mirrorfulImport = `const mirrorful = require('${
-        mirrorfulFolderPath === './' ? '..' : mirrorfulFolderPath
+        mirrorfulFolderPath === './' ? '.' : mirrorfulFolderPath
       }/.mirrorful/theme_cjs.js')\n`
     }
     await writeFile(
