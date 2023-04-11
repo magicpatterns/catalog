f001e0a3cf4c292a06995f76397faea599ca8729
teddarific <teddyni19@gmail.com>
2023-04-10 23:42:55 -0700

    Fix tailwind stuff

--- packages/core/src/translators/toJson.ts
+++ packages/core/src/translators/toJson.ts
@@ -1,7 +1,8 @@
 import { TPrimitives } from '@core/types'
+import JSON5 from 'json5'
 
 import { createThemeObject } from './createThemeObject'
 
 export const toJson = (primitives: TPrimitives): string => {
-  return JSON.stringify(createThemeObject(primitives), null, 2)
+  return JSON5.stringify(createThemeObject(primitives), null, 2)
 }
