f093a3114b4e66c8eb617e3e5984eabf56ea90e2
alexdanilowicz <alex.danilowicz@gmail.com>
2023-04-12 22:52:53 -0700

    Test server worfklow

--- server/src/services/imdb.ts
+++ server/src/services/imdb.ts
@@ -4,9 +4,7 @@ import { ImdbService } from '../api/generated/api/resources/imdb/service/ImdbSer
 export default new ImdbService({
   createMovie: (req, res) => {
     const id = req.body.title.toLowerCase().replaceAll(' ', '-')
-
     // TODO, add movie to database
-
     return res.send(id)
   },
   getMovie: (req, res) => {
