c5bfee62d965ad4857a937c690044d9d6044d0a9
alexdanilowicz <alex.danilowicz@gmail.com>
2023-04-12 22:35:20 -0700

    Test

---
+++ server/.eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/strict',
+    'plugin:@typescript-eslint/eslint-recommended',
+  ],
+  ignorePatterns: ['*.js', '*.cjs', 'src/api/generated'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+}
