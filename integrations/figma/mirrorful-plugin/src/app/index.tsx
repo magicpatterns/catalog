c24d842e54a3a37c8bbcf70f9f1beb2a80bfdea0
teddarific <teddyni19@gmail.com>
2023-04-13 13:58:20 -0700

    Arbitrary Figma updates

--- integrations/figma/mirrorful-plugin/src/app/index.tsx
+++ integrations/figma/mirrorful-plugin/src/app/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './components/App'
+import { App } from './components/App'
 
 document.addEventListener('DOMContentLoaded', function () {
   const container = document.getElementById('react-page')
