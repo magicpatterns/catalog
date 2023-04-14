9a445187763b3ddb9f34229dae4d13d8c15b50da
karanvirsb <karanvirboparai123@gmail.com>
2023-04-14 18:16:30 -0400

    added export

--- packages/ui-library/src/Buttons/Button.tsx
+++ packages/ui-library/src/Buttons/Button.tsx
@@ -3,8 +3,6 @@ import React from 'react'
 interface Button extends HTMLButtonElement {
   title: string
 }
-function Button(props: Button) {
+export function Button(props: Button) {
   return <button className={props.className}>{props.title}</button>
 }
-
-export default Button
