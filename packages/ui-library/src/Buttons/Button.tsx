07509a5d7ada3812d7109238f2ac0720baec2a61
karanvirsb <karanvirboparai123@gmail.com>
2023-04-14 18:31:58 -0400

    added chakra

--- packages/ui-library/src/Buttons/Button.tsx
+++ packages/ui-library/src/Buttons/Button.tsx
@@ -1,8 +1,10 @@
+import { Button as ChakraButton } from '@chakra-ui/react'
 import React from 'react'
 
 interface Button extends HTMLButtonElement {
   title: string
+  size: 'sm' | 'md' | 'lg'
 }
 export function Button(props: Button) {
-  return <button className={props.className}>{props.title}</button>
+  return <ChakraButton size={props.size}>{props.title}</ChakraButton>
 }
