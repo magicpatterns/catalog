6e9c2218608a9ac83f436d2df1816a7bd60b669d
teddarific <teddyni19@gmail.com>
2023-04-14 19:04:45 -0700

    Blocks

--- packages/blocks/.storybook/main.ts
+++ packages/blocks/.storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@chakra-ui/storybook-addon',
   ],
   framework: {
     name: '@storybook/react-webpack5',
