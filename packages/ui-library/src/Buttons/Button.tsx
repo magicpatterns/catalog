1e62126e979de89b8ce32ab0b6e04ac8a063bc43
karanvirsb <karanvirboparai123@gmail.com>
2023-04-14 20:16:54 -0400

    added partial

--- packages/ui-library/src/Buttons/Button.tsx
+++ packages/ui-library/src/Buttons/Button.tsx
@@ -1,10 +1,10 @@
 import { Button as ChakraButton } from '@chakra-ui/react'
 import React from 'react'
 
-interface Button extends HTMLButtonElement {
+interface Button extends Partial<HTMLButtonElement> {
   title: string
   size: 'sm' | 'md' | 'lg'
 }
-export function Button(props: Button) {
+export function Button(props: Button): JSX.Element {
   return <ChakraButton size={props.size}>{props.title}</ChakraButton>
 }
