6b8c7c6010bd8b559276b46314ca9304d0a7bd74
karanvirsb <karanvirboparai123@gmail.com>
2023-04-15 09:05:03 -0400

    removed size and added props

--- packages/blocks/src/Button/index.tsx
+++ packages/blocks/src/Button/index.tsx
@@ -5,13 +5,12 @@ import { ChakraProviderWrapper } from '../ChakraProviderWrapper'
 interface button extends ChakraProps {
   label: string
   onClick?: () => void
-  size?: 'sm' | 'md' | 'lg'
 }
 
 export function Button(props: button) {
   return (
     <ChakraProviderWrapper>
-      <ChakraButton onClick={props.onClick} size={props.size}>
+      <ChakraButton onClick={props.onClick} {...props}>
         {props.label}
       </ChakraButton>
     </ChakraProviderWrapper>
