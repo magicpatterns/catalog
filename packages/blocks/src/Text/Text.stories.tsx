9db0b4e69029c9dd9a69ac854b8bcb46de5d1c3a
karanvirsb <karanvirboparai123@gmail.com>
2023-04-16 18:25:55 -0400

    created extra small type

--- packages/blocks/src/Text/Text.stories.tsx
+++ packages/blocks/src/Text/Text.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import React from 'react'
 
-import { Text } from '.'
+import { Styles, Text } from '.'
 
 const meta: Meta<typeof Text> = {
   title: 'Text',
@@ -12,7 +12,27 @@ const meta: Meta<typeof Text> = {
 export default meta
 type Story = StoryObj<typeof Text>
 
-export const Default: Story = {
-  render: Text,
-  args: { label: 'Default', variants: 'md/normal' },
+export const Extra_Small = {
+  render: ({ styles }: { styles: Partial<Styles>[] }) => {
+    return (
+      <div>
+        {styles.map((style) => {
+          return <Text label={style} styles={style} key={style}></Text>
+        })}
+      </div>
+    )
+  },
+  args: {
+    styles: [
+      'xs/hairline',
+      'xs/thin',
+      'xs/light',
+      'xs/medium',
+      'xs/normal',
+      'xs/semibold',
+      'xs/bold',
+      'xs/extrabold',
+      'xs/black',
+    ] as Styles[],
+  },
 }
