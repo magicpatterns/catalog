5ab714c08909d29a58d0584d9f3a42438c34aa24
karanvirsb <karanvirboparai123@gmail.com>
2023-04-16 18:09:24 -0400

    added sizes and weights to create variants

--- packages/blocks/src/Text/index.tsx
+++ packages/blocks/src/Text/index.tsx
@@ -1,20 +1,44 @@
 import { Text as ChakraText } from '@chakra-ui/react'
-import { ChakraProviderWrapper } from '@ui/ChakraProviderWrapper'
 import React from 'react'
+
+import { ChakraProviderWrapper } from '../ChakraProviderWrapper'
+
+type Sizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+type Weights =
+  | 'hairline'
+  | 'thin'
+  | 'light'
+  | 'normal'
+  | 'medium'
+  | 'semibold'
+  | 'bold'
+  | 'extrabold'
+  | 'black'
+type Variants = `${Sizes}/${Weights}`
 
 export interface IText {
   label: string
+  variants: Variants
   icon?: React.ReactNode
   onClick?: () => void
 }
 
 export function Text(props: IText) {
+  const [size, weight] = extractVariant(props.variants)
   return (
     <ChakraProviderWrapper>
-      <ChakraText onClick={props.onClick} {...props}>
+      <ChakraText
+        onClick={props.onClick}
+        {...props}
+        fontSize={size}
+        fontWeight={weight}
+      >
         {props.icon ? props.icon : null}
         {props.label}
       </ChakraText>
     </ChakraProviderWrapper>
   )
+  function extractVariant(variants: string): [Sizes, Weights] {
+    return variants.split('/') as [Sizes, Weights]
+  }
 }
