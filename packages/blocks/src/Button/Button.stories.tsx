58bb9906aa1e12c01bc1c97147435684a1f6a268
karanvirsb <karanvirboparai123@gmail.com>
2023-04-16 15:51:49 -0400

    imported react and added tag for docs

--- packages/blocks/src/Button/Button.stories.tsx
+++ packages/blocks/src/Button/Button.stories.tsx
@@ -1,11 +1,13 @@
 import { EditIcon } from '@chakra-ui/icons'
 import type { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
 
 import { Button } from '.'
 
 const meta: Meta<typeof Button> = {
   title: 'Button',
   component: Button,
+  tags: ['autodocs'],
 }
 
 export default meta
