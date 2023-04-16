71691f7a36d077b23bb4baabe4dcb3ccb4b947ee
karanvirsb <karanvirboparai123@gmail.com>
2023-04-16 16:13:49 -0400

    added props

--- packages/blocks/src/Text/index.tsx
+++ packages/blocks/src/Text/index.tsx
@@ -2,10 +2,19 @@ import { Text as ChakraText } from '@chakra-ui/react'
 import { ChakraProviderWrapper } from '@ui/ChakraProviderWrapper'
 import React from 'react'
 
-export function Text() {
+export interface IText {
+  label: string
+  icon?: React.ReactNode
+  onClick?: () => void
+}
+
+export function Text(props: IText) {
   return (
     <ChakraProviderWrapper>
-      <ChakraText></ChakraText>
+      <ChakraText onClick={props.onClick} {...props}>
+        {props.icon ? props.icon : null}
+        {props.label}
+      </ChakraText>
     </ChakraProviderWrapper>
   )
 }
