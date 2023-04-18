461f10cc6fa8552164a8af1ac5714e73ca28e3a6
karanvirsb <karanvirboparai123@gmail.com>
2023-04-18 11:34:26 -0400

    adjusted hover

--- packages/blocks/src/Text/types.ts
+++ packages/blocks/src/Text/types.ts
@@ -1,3 +1,5 @@
+import { ChakraProps } from '@chakra-ui/react'
+
 export type Sizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
 export type Weights =
   | 'hairline'
@@ -11,18 +13,16 @@ export type Weights =
   | 'black'
 export type Styles = `${Sizes}/${Weights}`
 
-type main = {
-  variants?: 'hover'
+interface main extends ChakraProps {
   label: string
   styles: Styles
+  hover?: {
+    backgroundColor: string
+    color: string
+  }
   icon?: React.ReactNode
   onClick?: () => void
+  variants?: ''
 }
 
-export type IText =
-  | (main & { variants?: '' })
-  | (main & {
-      variants: 'hover'
-      backgroundColor: string
-      color: string
-    })
+export type IText = main
