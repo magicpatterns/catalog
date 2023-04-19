7f9b590fd86609d52d6e5c1007a34f86a9e5a50a
alexdanilowicz <alex.danilowicz@gmail.com>
2023-04-18 17:12:45 -0700

    changes

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -8,7 +8,7 @@ const PORT = 8080
 
 const app = express()
 
-app.use(express.json({ limit: '50mb' }))
+// app.use(express.json({ limit: '50mb' }))
 
 register(app, {
   registry: getRegistryService(),
