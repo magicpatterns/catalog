a8ffa96a871d84922a72a70e2cf94248dbb5609c
alexdanilowicz <alex.danilowicz@gmail.com>
2023-04-18 17:10:42 -0700

    SERVER

--- packages/server/src/api/generated/api/resources/commons/index.js
+++ packages/server/src/api/generated/api/resources/commons/index.js
@@ -22,27 +22,6 @@ var __createBinding =
         if (k2 === undefined) k2 = k
         o[k2] = m[k]
       })
-var __setModuleDefault =
-  (this && this.__setModuleDefault) ||
-  (Object.create
-    ? function (o, v) {
-        Object.defineProperty(o, 'default', { enumerable: true, value: v })
-      }
-    : function (o, v) {
-        o['default'] = v
-      })
-var __importStar =
-  (this && this.__importStar) ||
-  function (mod) {
-    if (mod && mod.__esModule) return mod
-    var result = {}
-    if (mod != null)
-      for (var k in mod)
-        if (k !== 'default' && Object.prototype.hasOwnProperty.call(mod, k))
-          __createBinding(result, mod, k)
-    __setModuleDefault(result, mod)
-    return result
-  }
 var __exportStar =
   (this && this.__exportStar) ||
   function (m, exports) {
@@ -51,8 +30,5 @@ var __exportStar =
         __createBinding(exports, m, p)
   }
 Object.defineProperty(exports, '__esModule', { value: true })
-exports.registry = exports.commons = void 0
-exports.commons = __importStar(require('./commons'))
-__exportStar(require('./commons/types'), exports)
-exports.registry = __importStar(require('./registry'))
-__exportStar(require('./registry/types'), exports)
+__exportStar(require('./types'), exports)
+__exportStar(require('./errors'), exports)
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,14 +1,17 @@
 import express from 'express'
 
 import { register } from './api/generated'
-import imdb from './services/imdb'
+
+import { getRegistryService } from './services/registry'
 
 const PORT = 8080
 
 const app = express()
 
+app.use(express.json({ limit: '50mb' }))
+
 register(app, {
-  imdb,
+  registry: getRegistryService(),
 })
 
 app.listen(PORT)
