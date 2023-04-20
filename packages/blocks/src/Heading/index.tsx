45119013b17dfdf04c8f4dc4a5a9ef80f430d125
karanvirsb <karanvirboparai123@gmail.com>
2023-04-19 20:01:01 -0400

    added variant props

--- packages/blocks/src/Heading/index.tsx
+++ packages/blocks/src/Heading/index.tsx
@@ -1,7 +1,28 @@
+import { ChakraProps, Heading as ChakraHeading } from '@chakra-ui/react'
 import React from 'react'
 
-function Heading() {
-  return <div>Heading</div>
+import { toCapitalize } from '../util/toCapitalize'
+
+type THeading = {
+  label: string
+  variants: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+  onClick?: () => void
+}
+
+function Heading(props: THeading) {
+  const variantProps: Record<THeading['variants'], ChakraProps> = {
+    h1: { fontSize: '2xl', fontWeight: 'black' },
+    h2: {},
+    h3: {},
+    h4: {},
+    h5: {},
+    h6: {},
+  }
+  return (
+    <ChakraHeading {...variantProps[props.variants]}>
+      {toCapitalize(props.label)}
+    </ChakraHeading>
+  )
 }
 
 export default Heading
