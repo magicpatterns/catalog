18805dd43733725f6759a479028b7daf15737988
alexdanilowicz <alex.danilowicz@gmail.com>
2023-04-19 18:20:12 -0700

    delete client

--- packages/client/src/api/resources/registry/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./CreateLibraryResponse";
-export * from "./LibraryId";
--- packages/client/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * as MirrorfulApi from "./api";
-export { MirrorfulApiClient } from "./Client";
-export { MirrorfulApiEnvironment } from "./environments";
-export { MirrorfulApiError, MirrorfulApiTimeoutError } from "./errors";
