d3b746aa58784631c6580d432546c795cfa70863
karanvirsb <karanvirboparai123@gmail.com>
2023-04-19 20:54:19 -0400

    creaed modal component

---
+++ packages/blocks/src/Modal/index.tsx
@@ -0,0 +1,51 @@
+import {
+  Modal as ChakraModal,
+  ModalCloseButton,
+  ModalFooter as ChakraModalFooter,
+  ModalHeader as ChakraModalHeader,
+} from '@chakra-ui/react'
+import React from 'react'
+
+import { Button } from '..'
+
+interface IModal {
+  isOpen: boolean
+  onClose: () => void
+  children: React.ReactNode
+  headerName: string
+  variants: 'save' | 'delete' | 'add'
+}
+export function Modal(props: IModal) {
+  return (
+    <ChakraModal isOpen={props.isOpen} onClose={props.onClose}>
+      <ModalHeader headerName={props.headerName} />
+      {props.children}
+    </ChakraModal>
+  )
+}
+
+export function ModalHeader({ headerName }: { headerName: string }) {
+  return (
+    <>
+      <ChakraModalHeader>{headerName}</ChakraModalHeader>
+      <ModalCloseButton />
+    </>
+  )
+}
+
+export function ModalFooter({
+  onClick,
+  variants,
+}: {
+  onClick: () => void
+  variants: IModal['variants']
+}) {
+  if (variants === 'save') {
+    return (
+      <ChakraModalFooter>
+        <Button label="cancel" variant="default"></Button>
+        <Button type="submit" label="save" variant="save"></Button>
+      </ChakraModalFooter>
+    )
+  }
+}
