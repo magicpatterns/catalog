7a12da3b4ae7c31a7e94303a11cfe9d0ef1a8c7b
karanvirsb <karanvirboparai123@gmail.com>
2023-04-19 20:07:57 -0400

    added all headings

--- packages/blocks/src/Heading/Heading.stories.tsx
+++ packages/blocks/src/Heading/Heading.stories.tsx
@@ -14,5 +14,30 @@ type Story = StoryObj<typeof Heading>
 
 export const Heading1: Story = {
   render: Heading,
-  args: { label: 'Heading1', variants: 'h1' },
+  args: { label: 'Heading 1', variants: 'h1' },
+}
+
+export const Heading2: Story = {
+  render: Heading,
+  args: { label: 'Heading 2', variants: 'h2' },
+}
+
+export const Heading3: Story = {
+  render: Heading,
+  args: { label: 'Heading 3', variants: 'h3' },
+}
+
+export const Heading4: Story = {
+  render: Heading,
+  args: { label: 'Heading 4', variants: 'h4' },
+}
+
+export const Heading5: Story = {
+  render: Heading,
+  args: { label: 'Heading 5', variants: 'h5' },
+}
+
+export const Heading6: Story = {
+  render: Heading,
+  args: { label: 'Heading 6', variants: 'h6' },
 }
