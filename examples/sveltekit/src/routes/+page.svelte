000a2beb090c7bda1e07a6016308157a2ec31eef
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2023-04-22 18:52:17 +0545

    Added vite config to import mirrorful theme

--- examples/sveltekit/src/routes/+page.svelte
+++ examples/sveltekit/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="header">Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<style>
+  @import '../mirrorful/theme.css';
+
+  .header {
+    color: var(--color-whiskey-900);
+  }
+</style>
