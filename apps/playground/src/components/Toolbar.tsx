743649513b8ee1c75e386db3d63d4b0a5ab5c99a
alexdanilowicz <alex.danilowicz@gmail.com>
2023-04-24 13:45:25 -0700

    Dropdown

--- apps/playground/src/components/Toolbar.tsx
+++ apps/playground/src/components/Toolbar.tsx
@@ -1,17 +1,53 @@
-import { Box, Button, Stack, Text, useToast } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Stack,
+  Text,
+  useToast,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
+  Flex,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+} from '@chakra-ui/react'
 import TriggerDevLogo from '../assets/triggerdev_logo.png'
 import { FiShare2, FiCloudLightning } from 'react-icons/fi'
-import { MoonIcon } from '@chakra-ui/icons'
+import { TbPlanet, TbShare, TbCheck } from 'react-icons/tb'
 import {
   MirrorfulApiClient,
   MirrorfulApiEnvironment,
 } from '@mirrorful-fern/api-client'
 import { useNavigate, useParams } from 'react-router-dom'
+import { useState } from 'react'
+
+const TRIGGER_DEV_ORG_ID = '535f2ra'
 
 export function Toolbar({ code }: { code: string }) {
   const { fileId, orgId } = useParams()
-  const toast = useToast()
   const navigate = useNavigate()
+  const [copyText, setCopyText] = useState<'Copy' | 'Copied'>('Copy')
+  const [isFocused, setIsFocused] = useState<boolean>(false)
+  const URL = `${window.location.origin}/${orgId}/${fileId}`
+
+  async function onCopy() {
+    // TODO(Danilowicz): there's a race condition here...
+    // We are assuming the navigate has already happened
+    setIsFocused(true)
+    await navigator.clipboard.writeText(URL)
+    setCopyText('Copied')
+    setTimeout(() => {
+      setCopyText('Copy')
+      setIsFocused(false)
+    }, 3000)
+  }
 
   async function onShare() {
     const environment =
@@ -22,7 +58,7 @@ export function Toolbar({ code }: { code: string }) {
       environment,
     })
 
-    const finalOrgId = orgId ? orgId : '535f2ra'
+    const finalOrgId = orgId ? orgId : TRIGGER_DEV_ORG_ID
     const finalFileId = fileId
       ? fileId
       : Math.floor(100000 + Math.random() * 900000).toString()
@@ -32,17 +68,6 @@ export function Toolbar({ code }: { code: string }) {
     })
 
     navigate(`/${finalOrgId}/${finalFileId}`)
-    navigator.clipboard.writeText(
-      `${window.location.origin}/${finalOrgId}/${finalFileId}`
-    )
-    toast({
-      title: 'Link copied.',
-      description: "We've saved your changes.",
-      status: 'success',
-      duration: 9000,
-      isClosable: true,
-      position: 'top-right',
-    })
   }
 
   return (
@@ -69,16 +94,88 @@ export function Toolbar({ code }: { code: string }) {
       </Box>
       <Box>
         <Stack direction="row" spacing={4}>
-          <Button
-            onClick={() => {
-              onShare()
-            }}
-            variant="primary"
-            size="compact"
-            leftIcon={<FiShare2 />}
-          >
-            Share
-          </Button>
+          <Popover>
+            <PopoverTrigger>
+              <Button
+                onClick={() => {
+                  onShare()
+                }}
+                variant="primary"
+                size="compact"
+                leftIcon={<TbShare />}
+              >
+                Share
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent
+              mx={3}
+              p={'5px'}
+              backgroundColor="bg"
+              borderColor={'divider'}
+            >
+              <PopoverBody
+                style={{ display: 'flex', flexDirection: 'column', gap: 8 }}
+              >
+                <Flex justifyContent={'flex-start'} alignItems="center">
+                  <TbPlanet color="white" />
+                  <Text ml="2" fontSize="14" fontWeight="bold" color="white">
+                    Share your sandbox
+                  </Text>
+                  <PopoverCloseButton color="white" />
+                </Flex>
+                <InputGroup size="sm">
+                  {/* <InputLeftAddon children="URL" /> */}
+                  <Input
+                    onFocus={() => {
+                      setIsFocused(true)
+                    }}
+                    onBlur={() => {
+                      setIsFocused(false)
+                    }}
+                    isReadOnly
+                    size="sm"
+                    borderColor={isFocused ? '#805AD5' : 'divider'}
+                    _hover={{ borderColor: '#805AD5' }}
+                    focusBorderColor={'#805AD5'}
+                    value={URL}
+                    color="white"
+                    style={{ cursor: 'pointer', borderRadius: '5px' }}
+                    onClick={async () => await onCopy()}
+                  />
+                </InputGroup>
+                <Box style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                  <Button
+                    disabled={copyText === 'Copied'}
+                    size="sm"
+                    backgroundColor={'bg'}
+                    borderColor={'divider'}
+                    borderWidth={'1px'}
+                    color={'playgroundText'}
+                    css={{
+                      minWidth: '110px', // no jumping when pressing copy
+                      backdropFilter: 'blur(2px)',
+                    }}
+                    _hover={{
+                      backgroundColor: 'bg',
+                    }}
+                    onClick={async () => await onCopy()}
+                  >
+                    {copyText === 'Copied' && (
+                      <>
+                        <Text mr={2} color="playgroundTextHover">
+                          Copied Link
+                        </Text>
+                        <TbCheck />
+                      </>
+                    )}
+                    {copyText === 'Copy' && (
+                      <Text color="white">Copy Link</Text>
+                    )}
+                  </Button>
+                </Box>
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
           {/* <Button
             variant="secondary"
             size="compact"
