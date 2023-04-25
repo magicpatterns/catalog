86570ea77a71c1ff63644e76eeabb6768151f165
karanvirsb <karanvirboparai123@gmail.com>
2023-04-25 10:49:15 -0400

    added styles for font size and weight

--- packages/blocks/src/Link/index.tsx
+++ packages/blocks/src/Link/index.tsx
@@ -2,24 +2,33 @@ import { ChakraProps, ChakraTheme, Link as ChakraLink } from '@chakra-ui/react'
 import NextLink from 'next/link'
 import React from 'react'
 
+import { extractVariant } from '..'
 import { ChakraProviderWrapper } from '../ChakraProviderWrapper'
+import { Styles } from '../Text/types'
 
 interface ILink
   extends Partial<
-    Pick<ChakraProps, 'bgColor' | 'color' | 'textDecoration' | 'textAlign'>
+    Pick<
+      ChakraProps,
+      'bgColor' | 'color' | 'textDecoration' | 'textAlign' | 'width'
+    >
   > {
   label: string
   href: string
   as: 'a' | typeof NextLink
+  variants?: Styles
   target?: '_blank' | '_parent' | '_top' | '_self'
   icon?: React.ReactNode
 }
 
 export function Link(props: ILink) {
+  const [Size, Weight] = extractVariant(props.variants ?? '')
   return (
     <ChakraProviderWrapper>
       <ChakraLink
         css={{ display: 'flex', gap: 8, alignItems: 'center' }}
+        fontSize={Size}
+        fontWeight={Weight}
         {...props}
       >
         {props.icon}
