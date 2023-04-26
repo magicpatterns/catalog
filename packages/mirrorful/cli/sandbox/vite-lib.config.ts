c0fd36e8328c3a9ae709a7ee436d5ea732ce0786
teddarific <teddyni19@gmail.com>
2023-04-26 11:23:20 -0700

    Bump packages

--- packages/mirrorful/cli/sandbox/vite-lib.config.ts
+++ packages/mirrorful/cli/sandbox/vite-lib.config.ts
@@ -3,6 +3,7 @@ import { resolve } from 'node:path'
 import dts from 'vite-plugin-dts'
 import react from '@vitejs/plugin-react'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import externalGlobals from 'rollup-plugin-external-globals'
 
 export default defineConfig(async ({ command, mode }) => {
   console.log(__dirname)
@@ -24,15 +25,24 @@ export default defineConfig(async ({ command, mode }) => {
       outDir: `${__dirname}/module`,
       emptyOutDir: true,
       minify: false,
+      // rollupOptions: {
+      //   external: ['react', 'react-dom'],
+      //   output: {
+      //     globals: {
+      //       react: 'React',
+      //       'react-dom': 'ReactDOM',
+      //     },
+      //   },
+      // },
     },
     define: {
       'process.env.NODE_ENV': '"production"',
     },
-    resolve: {
-      alias: {
-        react: 'https://esm.sh/react@18.2.0',
-        'react-dom': 'https://esm.sh/react-dom@18.2.0',
-      },
-    },
+    // resolve: {
+    //   alias: {
+    //     react: 'https://esm.sh/react@18.2.0?keep-names',
+    //     'react-dom': 'https://esm.sh/react-dom@18.2.0?keep-names',
+    //   },
+    // },
   }
 })
