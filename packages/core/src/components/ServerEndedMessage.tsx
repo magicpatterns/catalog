69c5c5ccada230026a7f460a480c4964fdad3547
karanvirsb <karanvirboparai123@gmail.com>
2023-05-01 20:39:33 -0400

    updated colors

--- packages/core/src/components/ServerEndedMessage.tsx
+++ packages/core/src/components/ServerEndedMessage.tsx
@@ -5,19 +5,49 @@ export default function ServerEndedMessage() {
   return (
     <Grid placeContent={'center'} minHeight="100svh" backgroundColor="#F2F2F2">
       <Text css={{ textAlign: 'center', fontSize: '5rem' }}>
-        Mirrorful Server is off.
+        Mirrorful Server is off!
       </Text>
       <Text css={{ textAlign: 'center', fontSize: '2rem', color: 'grey' }}>
         To start the server again, go to the terminal and type{' '}
-        <Text css={{ textAlign: 'center', color: 'purple' }} as="span">
+        <Text
+          css={{
+            textAlign: 'center',
+            color: '#ED8936',
+            backgroundColor: '#121212',
+            paddingInline: 4,
+            paddingBlock: 2,
+            marginInline: 4,
+          }}
+          as="span"
+        >
           yarn run mirrorful
         </Text>{' '}
         OR{' '}
-        <Text css={{ textAlign: 'center', color: 'purple' }} as="span">
+        <Text
+          css={{
+            textAlign: 'center',
+            color: '#ED8936',
+            backgroundColor: '#121212',
+            paddingInline: 4,
+            paddingBlock: 2,
+            marginInline: 4,
+          }}
+          as="span"
+        >
           npx mirrorful
-        </Text>
+        </Text>{' '}
         OR{' '}
-        <Text css={{ textAlign: 'center', color: 'purple' }} as="span">
+        <Text
+          css={{
+            textAlign: 'center',
+            color: '#ED8936',
+            backgroundColor: '#121212',
+            paddingInline: 4,
+            paddingBlock: 2,
+            marginInline: 4,
+          }}
+          as="span"
+        >
           pnpm mirrorful
         </Text>
       </Text>
