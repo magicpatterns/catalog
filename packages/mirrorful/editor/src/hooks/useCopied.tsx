c4bfcf8a5581fcdb1c79600903554053d20c3ffc
karanvirsb <karanvirboparai123@gmail.com>
2023-05-01 21:13:08 -0400

    created hook

---
+++ packages/mirrorful/editor/src/hooks/useCopied.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+
+export function useCopied() {
+  const [copied, setCopied] = useState(false)
+
+  useEffect(() => {
+    let copiedTimeout: NodeJS.Timeout
+
+    if (copied) {
+      copiedTimeout = setTimeout(() => {
+        setCopied(false)
+      }, 1500)
+    }
+
+    return () => clearTimeout(copiedTimeout)
+  }, [copied])
+
+  return { copied, setCopied }
+}
