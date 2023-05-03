18d5f8d09c2ce501408670c3bcc02dc96341103e
karanvirsb <karanvirboparai123@gmail.com>
2023-05-03 09:19:11 -0400

    added terminal text component

--- packages/mirrorful/editor/src/components/ServerEndedMessage.tsx
+++ packages/mirrorful/editor/src/components/ServerEndedMessage.tsx
@@ -3,10 +3,6 @@ import React from 'react'
 import { useCopied } from 'src/hooks/useCopied'
 
 export default function ServerEndedMessage() {
-  const { copied: yarnCopied, setCopied: setYarnCopied } = useCopied()
-  const { copied: npxCopied, setCopied: setNpxCopied } = useCopied()
-  const { copied: pnpmCopied, setCopied: setPnpmCopied } = useCopied()
-
   return (
     <Grid placeContent={'center'} minHeight="100svh" backgroundColor="#F2F2F2">
       <Text css={{ textAlign: 'center', fontSize: '5rem' }}>
@@ -14,90 +10,42 @@ export default function ServerEndedMessage() {
       </Text>
       <Text css={{ textAlign: 'center', fontSize: '2rem', color: 'grey' }}>
         To start the server again, go to the terminal and type{' '}
-        <Tooltip
-          label="Copied to Clipboard"
-          hasArrow
-          isDisabled={!yarnCopied}
-          isOpen={yarnCopied}
-        >
-          <Text
-            css={{
-              textAlign: 'center',
-              color: '#ED8936',
-              backgroundColor: '#121212',
-              paddingInline: 4,
-              paddingBlock: 2,
-              marginInline: 4,
-              borderRadius: 8,
-            }}
-            _hover={{ cursor: 'pointer' }}
-            as="span"
-            onClick={() =>
-              navigator.clipboard.writeText('yarn run mirrorful').then(() => {
-                setYarnCopied(true)
-              })
-            }
-          >
-            yarn run mirrorful
-          </Text>
-        </Tooltip>{' '}
-        OR{' '}
-        <Tooltip
-          label="Copied to Clipboard"
-          hasArrow
-          isDisabled={!npxCopied}
-          isOpen={npxCopied}
-        >
-          <Text
-            css={{
-              textAlign: 'center',
-              color: '#ED8936',
-              backgroundColor: '#121212',
-              paddingInline: 4,
-              paddingBlock: 2,
-              marginInline: 4,
-              borderRadius: 8,
-            }}
-            _hover={{ cursor: 'pointer' }}
-            as="span"
-            onClick={() =>
-              navigator.clipboard.writeText('npx mirrorful').then(() => {
-                setNpxCopied(true)
-              })
-            }
-          >
-            npx mirrorful
-          </Text>
-        </Tooltip>{' '}
-        OR{' '}
-        <Tooltip
-          label="Copied to Clipboard"
-          hasArrow
-          isDisabled={!pnpmCopied}
-          isOpen={pnpmCopied}
-        >
-          <Text
-            css={{
-              textAlign: 'center',
-              color: '#ED8936',
-              backgroundColor: '#121212',
-              paddingInline: 4,
-              paddingBlock: 2,
-              marginInline: 4,
-              borderRadius: 8,
-            }}
-            _hover={{ cursor: 'pointer' }}
-            as="span"
-            onClick={() =>
-              navigator.clipboard.writeText('pnpm mirrorful').then(() => {
-                setPnpmCopied(true)
-              })
-            }
-          >
-            pnpm mirrorful
-          </Text>
-        </Tooltip>
+        <TerminalText text="yarn run mirrorful"></TerminalText> OR{' '}
+        <TerminalText text="npx mirrorful"></TerminalText> OR{' '}
+        <TerminalText text="pnpm mirrorful"></TerminalText>
       </Text>
     </Grid>
+  )
+}
+function TerminalText({ text }: { text: string }) {
+  const { copied, setCopied } = useCopied()
+  return (
+    <Tooltip
+      label="Copied to Clipboard"
+      hasArrow
+      isDisabled={!copied}
+      isOpen={copied}
+    >
+      <Text
+        css={{
+          textAlign: 'center',
+          color: '#ED8936',
+          backgroundColor: '#121212',
+          paddingInline: 4,
+          paddingBlock: 2,
+          marginInline: 4,
+          borderRadius: 8,
+        }}
+        _hover={{ cursor: 'pointer' }}
+        as="span"
+        onClick={() =>
+          navigator.clipboard.writeText(text).then(() => {
+            setCopied(true)
+          })
+        }
+      >
+        {text}
+      </Text>
+    </Tooltip>
   )
 }
