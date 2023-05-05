7fb3ae2df09aab0ac0934b8d3507c067db718c9f
alexdanilowicz <alex.danilowicz@gmail.com>
2023-05-05 13:15:53 -0700

    changes

--- packages/mirrorful/editor/src/components/ServerEndedMessage.tsx
+++ packages/mirrorful/editor/src/components/ServerEndedMessage.tsx
@@ -6,10 +6,10 @@ export default function ServerEndedMessage() {
   return (
     <Grid placeContent={'center'} minHeight="100svh" backgroundColor="#F2F2F2">
       <Text css={{ textAlign: 'center', fontSize: '5rem' }}>
-        Mirrorful Server is off!
+        Your Mirrorful server is off.
       </Text>
       <Text css={{ textAlign: 'center', fontSize: '2rem', color: 'grey' }}>
-        To start the server again, go to the terminal and type{' '}
+        To start the server again, go to your terminal and type{' '}
         <TerminalText text="yarn run mirrorful"></TerminalText> OR{' '}
         <TerminalText text="npx mirrorful"></TerminalText> OR{' '}
         <TerminalText text="pnpm mirrorful"></TerminalText>
@@ -31,8 +31,7 @@ function TerminalText({ text }: { text: string }) {
           textAlign: 'center',
           color: '#ED8936',
           backgroundColor: '#121212',
-          paddingInline: 4,
-          paddingBlock: 2,
+          padding: 10,
           marginInline: 4,
           borderRadius: 8,
         }}
