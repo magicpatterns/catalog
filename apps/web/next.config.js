d73ea73855fd2616613d6eb96f72d109c6e52462
alexdanilowicz <alex.danilowicz@gmail.com>
2023-05-12 17:26:19 -0700

    test

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,6 +5,28 @@ const nextConfig = {
     externalDir: true,
   },
   transpilePackages: ['@mirrorful/core', '@mirrorful/blocks'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mirrorful-production.s3.us-west-1.amazonaws.com',
+        port: '',
+        pathname: '/assets/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mirrorful-production.s3.us-west-1.amazonaws.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- packages/core/src/components/Layout.tsx
+++ packages/core/src/components/Layout.tsx
@@ -92,15 +92,6 @@ export default function Layout({
 
   return (
     <>
-      <Head>
-        <title>Mirrorful Editor</title>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta
-          name="description"
-          content="Local editor for your design system"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Box css={{ width: '100%', minHeight: '100vh', display: 'flex' }}>
         <motion.div
           animate={{
