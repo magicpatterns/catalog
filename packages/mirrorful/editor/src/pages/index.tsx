1b111d6aa3eca0ccd0b942ec7cd63d4a6c6833b5
Alex Danilowicz <alex.danilowicz@gmail.com>
2023-05-12 17:46:49 -0700

    Add open graph, move assets to s3, align Head in local editor and web, and delete old Head, dont use next head. (#424)

--- packages/mirrorful/editor/src/pages/index.tsx
+++ packages/mirrorful/editor/src/pages/index.tsx
@@ -8,16 +8,35 @@ export default function Editor() {
     router.replace('/colors')
   }, [router])
 
+  // Note: the local editor is using pages dir, while web is using app dir
+  // So we have a <Head> here, but not in web.
   return (
     <>
       <Head>
         <title>Mirrorful Editor</title>
+        <meta property="og:title" content="Mirrorful" key="title" />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta
+          property="og:image"
+          content="https://mirrorful-production.s3.us-west-1.amazonaws.com/assets/components_graphic_dark.png"
+        />
+        <meta
+          property="og:description"
+          content="Create, edit, and manage your app's theme."
+        />
+        <meta
           name="description"
-          content="Local editor for your design system"
+          content="Create, edit, and manage your app's theme."
         />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="preload"
+          href="https://mirrorful-production.s3.us-west-1.amazonaws.com/assets/components_graphic_dark.png"
+        />
+        <link
+          rel="preload"
+          href="https://mirrorful-production.s3.us-west-1.amazonaws.com/assets/components_graphic_light.png"
+        />
       </Head>
     </>
   )
