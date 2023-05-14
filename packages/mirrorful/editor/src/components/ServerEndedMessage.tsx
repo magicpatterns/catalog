fd526bef405c1632179f4dac766ba1ea0d99432f
karanvirsb <karanvirboparai123@gmail.com>
2023-05-14 11:10:03 -0400

    updated with Code blocks and gradient

--- packages/mirrorful/editor/src/components/ServerEndedMessage.tsx
+++ packages/mirrorful/editor/src/components/ServerEndedMessage.tsx
@@ -1,17 +1,35 @@
-import { Grid, Text, Tooltip } from '@chakra-ui/react'
+import { Code, Grid, Text, Tooltip } from '@chakra-ui/react'
 import React from 'react'
 import { useCopied } from 'src/hooks/useCopied'
+import { CopyIcon } from '@chakra-ui/icons'
 
 export default function ServerEndedMessage() {
   return (
     <Grid placeContent={'center'} minHeight="100svh" backgroundColor="#F2F2F2">
       <Text css={{ textAlign: 'center', fontSize: '5rem' }}>
-        Your Mirrorful server is off.
+        Your{' '}
+        <Text
+          bgGradient="linear(to-b, hsla(6, 93%, 79%, 1), hsla(158, 67%, 64%, 1), hsla(197, 85%, 43%, 1))"
+          bgClip="text"
+          as="span"
+        >
+          Mirrorful{' '}
+        </Text>
+        Server is off.
       </Text>
-      <Text css={{ textAlign: 'center', fontSize: '2rem', color: 'grey' }}>
-        To start the server again, go to your terminal and type{' '}
-        <TerminalText text="yarn run mirrorful"></TerminalText> OR{' '}
-        <TerminalText text="npx mirrorful"></TerminalText> OR{' '}
+      <Text
+        css={{
+          textAlign: 'left',
+          fontSize: '2rem',
+          color: 'grey',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '2rem',
+        }}
+      >
+        To start the server again, type in your terminal:{' '}
+        <TerminalText text="yarn run mirrorful"></TerminalText>
+        <TerminalText text="npx mirrorful"></TerminalText>
         <TerminalText text="pnpm mirrorful"></TerminalText>
       </Text>
     </Grid>
@@ -20,31 +38,38 @@ export default function ServerEndedMessage() {
 function TerminalText({ text }: { text: string }) {
   const { copied, setCopied } = useCopied()
   return (
-    <Tooltip
-      label="Copied to Clipboard"
-      hasArrow
-      isDisabled={!copied}
-      isOpen={copied}
-    >
-      <Text
+    <>
+      <Code
         css={{
-          textAlign: 'center',
-          color: '#ED8936',
-          backgroundColor: '#121212',
           padding: 10,
           marginInline: 4,
           borderRadius: 8,
+          textAlign: 'left',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          fontSize: '1.5rem',
         }}
-        _hover={{ cursor: 'pointer' }}
-        as="span"
-        onClick={() =>
-          navigator.clipboard.writeText(text).then(() => {
-            setCopied(true)
-          })
-        }
+        variant="solid"
+        colorScheme={'gray'}
       >
         {text}
-      </Text>
-    </Tooltip>
+        <Tooltip
+          label="Copied to Clipboard"
+          hasArrow
+          isDisabled={!copied}
+          isOpen={copied}
+        >
+          <CopyIcon
+            css={{ cursor: 'pointer' }}
+            onClick={() =>
+              navigator.clipboard.writeText(text).then(() => {
+                setCopied(true)
+              })
+            }
+          />
+        </Tooltip>
+      </Code>
+    </>
   )
 }
