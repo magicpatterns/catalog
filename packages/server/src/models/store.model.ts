ec7249aaf4f0ac001b0b33bae8b6423962cdd07a
teddarific <teddyni19@gmail.com>
2023-05-16 01:02:06 -0700

    Update models and types

--- packages/server/src/models/store.model.ts
+++ packages/server/src/models/store.model.ts
@@ -5,6 +5,7 @@ import {
   TPrimitives,
   TPrimitivesTypography,
   TTheme,
+  TMetadata,
 } from './types'
 
 const typographyPrimitivesSchema = new mongoose.Schema<TPrimitivesTypography>({
@@ -25,6 +26,10 @@ const themeSchema = new mongoose.Schema<TTheme>({
   tokens: { type: Map, of: Object }, // tokens is really TTokenGroup, but the keys are dynamic... so we let is be free-form
 })
 
+const metadataSchema = new mongoose.Schema<TMetadata>({
+  completedOnboardings: { type: [String], default: [], _id: false },
+})
+
 const storeSchema = new mongoose.Schema<
   TMirrorfulStore & { id: string; orgId: string; lastUpdatedByUserId?: string }
 >({
@@ -34,6 +39,7 @@ const storeSchema = new mongoose.Schema<
   primitives: { type: primitivesSchema, _id: false },
   themes: { type: [themeSchema], _id: false },
   files: { type: [String], _id: false }, // 'css' | 'scss' | 'js' | 'cjs' | 'ts' | 'json'
+  metadata: { type: metadataSchema, _id: false },
 })
 
 export const Store = mongoose.model('Store', storeSchema)
