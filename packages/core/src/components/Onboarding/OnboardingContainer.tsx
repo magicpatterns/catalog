23a43c22c09fd1c45ade29efcda405d484774611
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-05-19 06:51:41 +0530

    fix:set a min-height for smaller mobile devices

--- packages/core/src/components/Onboarding/OnboardingContainer.tsx
+++ packages/core/src/components/Onboarding/OnboardingContainer.tsx
@@ -16,6 +16,10 @@ export function OnboardingContainer({
         justifyContent: 'center',
         alignItems: 'center',
       }}
+      minHeight={{
+        base: '896px',
+        md: 'unset',
+      }}
     >
       <Box
         css={{
